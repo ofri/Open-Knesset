a19e58b7433493bea2c714fc1a9127c08d562c09
Benny Daon <bennydaon@gmail.com>
2009-08-13 16:52:37 +0300

    member interface works. try: http://127.0.0.1:8000/api/member/42/

--- src/knesset/api/handlers.py
+++ src/knesset/api/handlers.py
@@ -1,9 +1,25 @@
 from piston.handler import BaseHandler
-from knesset.simple.models import Vote
+from knesset.simple.models import Vote, Member, Membership
+
+class MemberHandler(BaseHandler):
+    fields = ('id', 'name', 'start_date', 'end_date',
+              'membership',
+             )
+    allowed_methods = ('GET',)
+    model = Member
+    def read(self, request, member_id):
+        member = Member.objects.get(pk=member_id)
+        return member
+
+    @classmethod
+    def membership (self, member):
+        qs = Membership.objects.filter(member=member)
+        return qs.values('start_date', 'end_date', 'party_id')
+
 
 class VoteHandler(BaseHandler):
     # fields = ('for', 'against', 'abstain')
-    fields = ('id', 'time', 'ForVotesCount', 'AgainstVotesCount', 
+    fields = ('id', 'title', 'time', 'ForVotesCount', 'AgainstVotesCount',
               ('voted_for' , ('member', ('id'))),
               ('voted_against' , ('member', ('id'))),
               ('voted_abstain' , ('member', ('id'))),
@@ -12,7 +28,7 @@ class VoteHandler(BaseHandler):
               'topics_against',
              )
     allowed_methods = ('GET',)
-    model = Vote   
+    model = Vote
     def read(self, request, vote_id):
         vote = Vote.objects.get(pk=vote_id)
         return vote
--- src/knesset/api/urls.py
+++ src/knesset/api/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from knesset.api.handlers import VoteHandler
+from knesset.api.handlers import VoteHandler, MemberHandler
 
 vote_handler = Resource(VoteHandler)
+member_handler = Resource(MemberHandler)
 
 urlpatterns = patterns('',
       url(r'^vote/(?P<vote_id>[^/]+)/',
           vote_handler,
+          { 'emitter_format': 'json' }),
+      url(r'^member/(?P<member_id>[^/]+)/',
+          member_handler,
           { 'emitter_format': 'json' }),
       )
 
