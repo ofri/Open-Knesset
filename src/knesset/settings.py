58ec0b8125cc61922986ac89c8b07b48798c8a67
Benny Daon <bennydaon@gmail.com>
2009-08-20 01:15:38 +0300

    Hope page first version!

--- src/knesset/settings.py
+++ src/knesset/settings.py
@@ -29,7 +29,7 @@ TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'he'
 
 SITE_ID = 1
 
