66a565324db80123865090ff954941fb7b79de01
Benny Daon <bennydaon@gmail.com>
2009-08-27 09:34:15 +0300

    Adding partiesrties should now work

--- static/js/states.js
+++ static/js/states.js
@@ -9,12 +9,23 @@ var Members = function ()  {
     };
     return i;
 };
+
 var PastVotes = function ()  {
     var i = new BreadCrumb();
     i.name = 'vote';
     i.nav_id = NAV_SUFFIX + 'past-votes';
     i.one_liner = function (item) {
         return [item.time, item.title].join("\t");
+    };
+    return i;
+};
+
+var Parties = function ()  {
+    var i = new BreadCrumb();
+    i.name = 'party';
+    i.nav_id = NAV_SUFFIX + 'parties';
+    i.one_liner = function (item) {
+        return item.name
     };
     return i;
 };
