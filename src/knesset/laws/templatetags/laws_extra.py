6b92ba1631a1d3bee99bab6beccfe62763c61557
ofri <ofri.raviv@gmail.com>
2010-02-03 23:26:54 +0200

    added tagged view, and general fixes.

--- src/knesset/laws/templatetags/laws_extra.py
+++ src/knesset/laws/templatetags/laws_extra.py
@@ -12,8 +12,6 @@ def user_votes(user, vote, tag):
         cv = tv.vote
     except Exception:
         cv = 0
-    print cv==1
-    print cv==-1
     return {'user': user, 'vote':vote, 'tag':tag, 'current_vote_up':cv==1, 'current_vote_down':cv==-1}
 
 
--- src/knesset/laws/urls.py
+++ src/knesset/laws/urls.py
@@ -1,11 +1,12 @@
 #encoding: UTF-8
 from django.conf.urls.defaults import *
+from django.utils.translation import ugettext
 from knesset.hashnav.views import ListDetailView
 from models import Vote
 from views import *
 
-vote_view = ListDetailView(queryset = Vote.objects.all(),paginate_by=20) # filter(end_date__gte=yearstart(2009)),
-law_approve_view = ListDetailView(queryset = Vote.objects.all().filter(title__contains='אישור החוק'),paginate_by=20)
+vote_view = ListDetailView(queryset = Vote.objects.all(),paginate_by=20, extra_context={'votes':True,'title':ugettext('Votes')}) # filter(end_date__gte=yearstart(2009)),
+law_approve_view = ListDetailView(queryset = Vote.objects.all().filter(title__contains='אישור החוק'),paginate_by=20, extra_context={'law_approve':True, 'title':ugettext('Law Approvals')})
 
 lawsurlpatterns = patterns ('',
     url(r'^law-approve/$', law_approve_view, name='law-approve'),
@@ -13,4 +14,5 @@ lawsurlpatterns = patterns ('',
     url(r'^vote/(?P<object_id>\d+)/submit-tags/$', submit_tags),
     url(r'^vote/(?P<object_id>\d+)/tag-votes/(?P<tag_id>\d+)/(?P<vote>[+\-\d]+)/$', vote_on_tag),  
     url(r'^vote/(?P<object_id>\d+)/$', vote_view, name='vote-detail'),
+    url(r'^votes/tagged/(?P<tag>.*)/$', tagged, name='tagged-votes'),    
 )
