cf44aba26381e26efecb0135a0f5c0065d4e65f9
ofri <ofri.raviv@gmail.com>
2010-02-14 23:52:13 +0200

    separated mks to current and past mks.

--- src/knesset/mks/urls.py
+++ src/knesset/mks/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from knesset.mks.models import Member, Party
 from knesset.hashnav.views import ListDetailView
                        
-member_view = ListDetailView(queryset = Member.objects.all(),) # filter(end_date__gte=yearstart(2009)),
+member_view = ListDetailView(queryset = Member.objects.all(),extra_context = {'past_mks':Member.objects.filter(is_current=False)}) 
 party_view = ListDetailView(queryset = Party.objects.all(),) 
 mksurlpatterns = patterns('knesset.mks.views',
     url(r'^member/$', member_view, name='member-list'),
--- src/knesset/simple/management/commands/syncdata.py
+++ src/knesset/simple/management/commands/syncdata.py
@@ -162,7 +162,7 @@ class Command(NoArgsCommand):
 
         for id in range(self.last_downloaded_member_id+1,900): # TODO - find max member id in knesset website and use here
             m = mk_parser.MKHtmlParser(id).Dict
-            if (m.has_key('name') and m['name'] != None): name = m['name'].encode(ENCODING).replace('&nbsp;',' ')
+            if (m.has_key('name') and m['name'] != None): name = m['name'].encode(ENCODING).replace('&nbsp;',' ').replace(u'\xa0',' ')
             else: continue
             f.write("%d\t%s\t" % (  id, name ))
             for field in fields:
