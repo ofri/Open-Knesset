c7792bb48daf0fdcf879a44d834d7f69e0275c35
Benny Daon <bennydaon@gmail.com>
2010-03-16 17:12:49 +0200

    API improvements - still not there...

--- src/knesset/api/handlers.py
+++ src/knesset/api/handlers.py
@@ -2,8 +2,8 @@ from datetime import datetime
 from piston.handler import BaseHandler
 from knesset.mks.models import Member, Party, Membership
 from knesset.laws.models import Vote, VoteAction
-from knesset.utils import yearstart, yearend
 
+DEFAULT_PAGE_LEN = 20
 def limit_by_request(qs, request):
     if 'num' in request.GET:
         num = int(request.GET['num'])
@@ -12,81 +12,83 @@ def limit_by_request(qs, request):
     return qs
 
 class MemberHandler(BaseHandler):
-    fields = ('id', 'name', 'start_date', 'end_date',
-              'membership',)
+    fields = ('url', 'name', 
+              'member',)
     allowed_methods = ('GET',)
     model = Member
-    def read(self, request, member_id=None):
-        if member_id:
-            qs = Member.objects.filter(pk=member_id)
+    qs = Membership.objects.all()
+    def read(self, request, **kwargs):
+        if member_id in kwargs:
+            qs = self.qs.filter(pk=kwargs[member_id])
         else:
-            year = request.GET.get('year', -1)
-            if year == -1:
-                year = datetime.now().year
-            qs = Member.objects.filter(end_date__gte=yearstart(2009))
+            year = request.GET.get('year', None)
+            if year:
+                qs = self.qs.filter(end_date__gte=yearstart(year))
         return qs
 
     @classmethod
-    def membership (self, member):
-        qs = Membership.objects.filter(member=member)
-        return [dict(since=o.start_date, until=o.end_date,
-                     id=o.party.uri_template) for o in qs]
+    def url (self, member):
+        return member.get_absolute_url()
 
-class VoteDetailsHandler(BaseHandler):
-    # fields = ('for', 'against', 'abstain')
-    fields = ('id', 'title', 'time', 'for_votes_counts', 'against_votes_counts',
-              'member_uri_template', 
-              'voted_for',
-              'voted_against' ,
-              'voted_abstain' ,
-              'didnt_vote' ,
-              'topics_for',
-              'topics_against',
+    @classmethod
+    def member (self, member):
+        qs = self.qs.filter(member=member)
+        return map(lambda o: dict(url=o.party.get_absolute_url(),
+                     name=o.party.name,
+                     since=o.start_date,
+                     until=o.end_date,
+                     ), qs)
+
+class VoteHandler(BaseHandler):
+    fields = ('url', 'title', 'time', 
               'summary',
+              'for_votes', 'agains_votes' , 'abstain_votes' , 'didnt_vote' ,
+              'topics_for', 'topics_against',
               'full_text_url',
              )
+    exclude = ('member')
     allowed_methods = ('GET',)
     model = Vote
-    def read(self, request, vote_id):
-        return Vote.objects.filter(pk=vote_id)
+    qs = Vote.objects.all()
+
+    def read(self, request, **kwargs):
+        ''' returns a vote or a list of votes '''
+        qs = self.qs
+
+        if 'id' in kwargs:
+            return super(VoteHandler, self).read(request, **kwargs)
+
+        type = request.GET.get('type', None)
+        order = request.GET.get('order', None)
+        days_back = request.GET.get('days_back', None)
+        page_len = request.GET.get('page_len', DEFAULT_PAGE_LEN)
+        page_num=request.GET.get('page_num', 0)
+
+        if type:
+            qs = qs.filter(title__contains=type)
+        if days_back:
+            qs = qs.since(days=days_back)
+        if order:
+            qs = qs.sort(by=order)
+        return qs[page_len*page_num:page_len*(page_num +1)]
 
     @classmethod
-    def member_uri_template(self, vote):
-        # TODO: needs Drying - use django's url resolver
-        return "/member/{id}/htmldiv/" 
+    def url(self, vote):
+        return vote.get_absolute_url()
 
     @classmethod
-    def voted_for(self, vote):
-        return vote.get_voters_id('for')
-
-    @classmethod
-    def voted_against(self, vote):
-        return vote.get_voters_id('against')
-
-    @classmethod
-    def voted_abstain(self, vote):
+    def abstain_votes(self, vote):
         return vote.get_voters_id('abstain')
 
     @classmethod
     def didnt_vote(self, vote):
         return vote.get_voters_id('no-vote')
 
-class VoteHandler(BaseHandler):
-    # fields = ('for', 'against', 'abstain')
-    fields = ('id', 'title', 'time', 'for_votes_count', 'against_vote_count',
-              'topics_for',
-              'topics_against',
-             )
-    allowed_methods = ('GET',)
-    model = Vote
-    def read(self, request):
-        qs = Vote.objects.all().order_by('-time')
-        return limit_by_request(qs, request)
-
 class PartyHandler(BaseHandler):
     fields = ('id', 'name', 'start_date', 'end_date')
     allowed_methods = ('GET',)
     model = Party
+    
     def read(self, request, party_id=None):
         if party_id:
             return Party.objects.filter(pk=party_id)
--- src/knesset/api/urls.py
+++ src/knesset/api/urls.py
@@ -2,21 +2,15 @@ from django.conf.urls.defaults import *
 from piston.resource import Resource
 from piston.emitters import Emitter
 
-from knesset.emitters import DivEmitter
 from knesset.api.handlers import *
 
-Emitter.register('htmldiv', DivEmitter, 'text/html; charset=utf-8')
-
 vote_handler = Resource(VoteHandler)
-vote_details_handler = Resource(VoteDetailsHandler)
 member_handler = Resource(MemberHandler)
 party_handler = Resource(PartyHandler)
 
-context = dict(emitter_format='json')
-
 urlpatterns = patterns('',
-      url(r'^vote/(?P<emitter_format>\w+)/$', vote_handler),
-      url(r'^vote/(?P<vote_id>[^/]+)/(?P<emitter_format>\w+)/$', vote_details_handler),
+      url(r'^vote/$', vote_handler),
+      url(r'^vote/(?P<id>[0-9]+)/$', vote_handler),
       url(r'^member/(?:(?P<member_id>[^/]+)/)?(?P<emitter_format>.+)/$', member_handler),
       url(r'^party/(?:(?P<party_id>[^/]+)/)?(?P<emitter_format>.+)/$', party_handler),
       )
