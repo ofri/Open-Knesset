0a079b7045cf114473f71a2a3490e0ac044c9104
Benny Daon <bennydaon@gmail.com>
2010-04-14 16:15:58 +0300

    tests cleanup. bin/django test now always OK. It fails, but the same tests pass when run on their own - 'bin/django test user'. maybe *user* was not the best name for an app?

--- src/knesset/user/models.py
+++ src/knesset/user/models.py
@@ -1,24 +1,27 @@
 #encoding: utf-8
 from django.db import models
-from knesset.mks.models import Party, Member
 from django.utils.translation import ugettext_lazy as _
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.conf import settings
+
+from knesset.mks.models import Party, Member
 
 class UserProfile(models.Model):
     ''' 
     This model is extending the builtin user model.  
     The extension includes a list of followed parties and members.
 
-    >>> user = User.objects.create(username='daonb')
-    >>> profile = user.get_profile()
+    >>> daonb = User.objects.create(username='daonb')
+    >>> profile = daonb.get_profile()
     >>> legalize = Party.objects.create(name='legalize')
     >>> profile.followed_parties.add(legalize)
-    >>> legalize == user.get_profile().parties[0]
+    >>> legalize == daonb.get_profile().parties[0]
     True
     >>> dbg = Member.objects.create(name='david ben gurion')
     >>> profile.followed_members.add(dbg)
-    >>> dbg = user.get_profile().members[0]
+    >>> dbg == daonb.get_profile().members[0]
+    True
 
     '''
 
