e3f8ed30dbb0967028ff78f167d7690701fd9285
ofri <ofri.raviv@gmail.com>
2010-05-04 22:28:15 +0300

    some DRY in user urls

--- src/knesset/user/urls.py
+++ src/knesset/user/urls.py
@@ -10,12 +10,15 @@ urlpatterns = patterns('knesset.user.views',
 # views coded elsewhere
 urlpatterns += patterns('',
     (r'^registration/', include('knesset.accounts.urls')),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'user/login.html'}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
-    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', {'template_name': 'user/password_reset_form.html'}, name='password_reset'),
-    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'user/password_reset_done.html'}),
-    url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'user/password_reset_confirm.html'}),
-    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'user/password_reset_complete.html'}),
+    )
+
+urlpatterns += patterns('django.contrib.auth.views',
+    url(r'^login/$', 'login', {'template_name': 'user/login.html'}, name='login'),
+    url(r'^logout/$', 'logout_then_login', name='logout'),
+    url(r'^password_reset/$', 'password_reset', {'template_name': 'user/password_reset_form.html'}, name='password_reset'),
+    url(r'^password_reset/done/$', 'password_reset_done', {'template_name': 'user/password_reset_done.html'}),
+    url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm', {'template_name': 'user/password_reset_confirm.html'}),
+    url(r'^reset/done/$', 'password_reset_complete', {'template_name': 'user/password_reset_complete.html'}),
 
 
     )
