3b7d2a738748f69cf0348e0e28614435b9ce9482
Benny Daon <bennydaon@gmail.com>
2010-06-16 14:46:05 +0300

    add rss type to feed<->member connection

--- src/knesset/mks/listeners.py
+++ src/knesset/mks/listeners.py
@@ -1,17 +1,21 @@
+#encoding: utf-8
 from django.db.models.signals import post_save
 from planet.models import Feed, Post
 from actstream import action
 from knesset.utils import cannonize, disable_for_loaddata
 from knesset.laws.models import VoteAction
-from knesset.links.models import Link
+from knesset.links.models import Link, LinkType
+from knesset.mks.models import Member
 
 @disable_for_loaddata
 def connect_feed(sender, created, instance, **kwargs):
     if created:
         t = cannonize(instance.title)
+        rss_type = LinkType.objects.get(title='רסס')
         for m in Member.objects.all():
             if t.rfind(cannonize(m.name)) != -1:
-                Link.objects.create(url=instance.url, title = instance.title, content_object=m)
+                Link.objects.create(url=instance.url, title = instance.title,
+                                    content_object=m, link_type=rss_type)
                 print u'connected feed to %s' % m
                 return
 post_save.connect(connect_feed, sender=Feed)
