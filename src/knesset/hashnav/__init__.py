c00d2363ad49d487bee71085a32adb25fd7fa6c0
Benny Daon <bennydaon@gmail.com>
2010-07-29 22:52:51 +0300

    adding method_decorator to enable the use of login_required

--- src/knesset/hashnav/__init__.py
+++ src/knesset/hashnav/__init__.py
@@ -1,3 +1,4 @@
+from functools import wraps, update_wrapper
 from base import View
 from list import ListView
 from detail import DetailView
@@ -6,3 +7,31 @@ from dates import (ArchiveView, YearView, MonthView,
                                      DateDetailView)
 from edit import (FormView, CreateView, UpdateView,
                                     DeleteView)
+def method_decorator(decorator):
+    """
+    Converts a view function decorator into a method decorator.
+    the special thing about view decorators is that they expect 
+    'request' to be their first argument.
+    Example:
+
+        class MyView(View):
+            allowed_methods = ['GET','POST'],
+
+            @method_decorator(login_required)
+            def POST(*arg, **kwargs):
+                ...
+    """
+    def _dec(func):
+        def _wrapper(self, *args, **kwargs):
+            def bound_func(request, *args2, **kwargs2):
+                return func(self, *args2, **kwargs2)
+            # bound_func has the signature that 'decorator' expects i.e.  no
+            # 'self' argument, but it is a closure over self so it can call
+            # 'func' correctly.
+            return decorator(bound_func)(self.request, *args, **kwargs)
+        return wraps(func)(_wrapper)
+    update_wrapper(_dec, decorator)
+    # Change the name to aid debugging.
+    _dec.__name__ = 'method_dec(%s)' % decorator.__name__
+    return _dec
+
