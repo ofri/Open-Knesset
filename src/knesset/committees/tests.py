7dab3916397daea3f3ba2260f64ac30585fd4c0e
Benny Daon <bennydaon@gmail.com>
2010-08-03 15:57:23 +0300

    committee refactoring complete with unit testing

--- src/knesset/committees/tests.py
+++ src/knesset/committees/tests.py
@@ -1,23 +1,81 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
+from datetime import datetime
 from django.test import TestCase
+from django.core.exceptions import ImproperlyConfigured
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from knesset.laws.models import Bill
+from knesset.mks.models import Member
+from models import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+just_id = lambda x: x.id
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class ListViewTest(TestCase):
 
->>> 1 + 1 == 2
-True
-"""}
+    def setUp(self):
+        self.committee_1 = Committee.objects.create(name='c1')
+        self.committee_2 = Committee.objects.create(name='c2')
+        self.meeting_1 = self.committee_1.meetings.create(date=datetime.now(),
+                                                         protocol_text='m1')
+        self.meeting_2 = self.committee_1.meetings.create(date=datetime.now(),
+                                                         protocol_text='m2')
+        self.jacob = User.objects.create_user('jacob', 'jacob@example.com',
+                                              'JKM')
+        self.bill_1 = Bill.objects.create(stage='1', title='bill 1')
+        self.mk_1 = Member.objects.create(name='mk 1')
+
+    def testCommitteeList(self):
+        res = self.client.get(reverse('committee-list'))
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, 'committees/committee_list.html')
+        object_list = res.context['object_list']
+        self.assertEqual(map(just_id, object_list), 
+                         [ self.committee_1.id, self.committee_2.id, ])
+
+    def testCommitteeMeetings(self):
+        res = self.client.get(reverse('committee-detail',
+                                 kwargs={'committee_id': self.committee_1.id}))
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res,
+                                'committees/committeemeeting_list.html')
+        object_list = res.context['object_list']
+        self.assertEqual(map(just_id, object_list), 
+                         [self.meeting_1.id, self.meeting_2.id, ], 
+                         'object_list has wrong objects: %s' % object_list)
+
+    def testLoginRequired(self):
+        res = self.client.post(reverse('committee-meeting',
+                           kwargs={'object_id': self.meeting_1.id}))
+        self.assertFalse(self.bill_1 in self.meeting_1.bills_first.all())
+        self.assertEqual(res.status_code, 302)
+        self.assertTrue(res['location'].startswith('http://testserver/user/login/'))
+
+    def testConnectToMK(self):
+        self.assertTrue(self.client.login(username='jacob', password='JKM'))
+        res = self.client.post(reverse('committee-meeting',
+                           kwargs={'object_id': self.meeting_1.id}),
+                               {'user_input_type': 'mk',
+                                'mk_name': self.mk_1.name})
+        self.assertEqual(res.status_code, 302)
+        self.assertTrue(self.meeting_1 in self.mk_1.committee_meetings.all())
+        self.client.logout()
+
+    def testConnectToBill(self):
+        self.assertTrue(self.client.login(username='jacob', password='JKM'))
+        res = self.client.post(reverse('committee-meeting',
+                                       kwargs={'object_id':
+                                               self.meeting_1.id}),
+                               {'user_input_type': 'bill',
+                                'bill_id': self.bill_1.id})
+        self.assertEqual(res.status_code, 302)
+        self.assertTrue(self.bill_1 in self.meeting_1.bills_first.all())
+        self.client.logout()
+
+    def tearDown(self):
+        self.meeting_1.delete()
+        self.meeting_2.delete()
+        self.committee_1.delete()
+        self.committee_2.delete()
+        self.jacob.delete()
+        self.bill_1.delete()
+        self.mk_1.delete()
 
--- src/knesset/committees/views.py
+++ src/knesset/committees/views.py
@@ -7,59 +7,57 @@ import difflib
 import datetime
 import re
 
-from knesset.hashnav import ListView, DetailView
+from knesset.hashnav import ListView, DetailView, method_decorator
 from knesset.laws.models import Bill, PrivateProposal
+from django.shortcuts import get_object_or_404
 from models import *
 
 class MeetingDetailView(DetailView):
 
-    allowed_methods = ['GET', 'POST'],
+    allowed_methods = ('GET', 'POST')
 
-    @login_required
+    @method_decorator(login_required)
     def POST(self, object_id, **kwargs):
+        cm = get_object_or_404(CommitteeMeeting, pk=object_id)
         bill = None
         request = self.request
-        try:
-            user_input_type = request.POST.get('user_input_type')
-            if user_input_type == 'bill':
-                bill_id = request.POST.get('bill_id')
-                if bill_id.isdigit(): 
-                    bill = Bill.objects.get(pk=bill_id)
-                else: # not a number, maybe its p/1234
-                    m = re.findall('\d+',bill_id)
-                    if len(m)!=1:
-                        raise ValueError("didn't find exactly 1 number in bill_id=%s" % bill_id)
-                    pp = PrivateProposal.objects.get(proposal_id=m[0])
-                    bill = pp.bill                    
-                
-                if bill.stage in ['1','2','-2','3']: # this bill is in early stage, so cm must be one of the first meetings
-                    bill.first_committee_meetings.add(cm)
-                else: # this bill is in later stages
-                    v = bill.first_vote # look for first vote
-                    if v and v.time.date() < cm.date: # and check if the cm is after it,            
-                        bill.second_committee_meetings.add(cm) # if so, this is a second committee meeting
-                    else: # otherwise, assume its first cms.
-                        bill.first_committee_meetings.add(cm)
-                bill.update_stage()
-                action.send(request.user, verb='merged',
-                    description=cm,
-                    target=bill,
-                    timestamp=datetime.datetime.now())
-                
-            if user_input_type == 'mk':
-                mk_names = Member.objects.values_list('name',flat=True)
-                mk_name = difflib.get_close_matches(request.POST.get('mk_name'), mk_names)[0]
-                mk = Member.objects.get(name=mk_name)
-                cm.mks_attended.add(mk)
-                cm.save() # just to signal, so the attended Action gets created.
-                action.send(request.user, verb='merged',
-                    description=cm,
-                    target=mk,
-                    timestamp=datetime.datetime.now())
+        user_input_type = request.POST.get('user_input_type')
+        if user_input_type == 'bill':
+            bill_id = request.POST.get('bill_id')
+            if bill_id.isdigit():
+                bill = get_object_or_404(Bill, pk=bill_id)
+            else: # not a number, maybe its p/1234
+                m = re.findall('\d+',bill_id)
+                if len(m)!=1:
+                    raise ValueError("didn't find exactly 1 number in bill_id=%s" % bill_id)
+                pp = PrivateProposal.objects.get(proposal_id=m[0])
+                bill = pp.bill
 
-        except Exception,e:
-            logger.error(e)
-        
+            if bill.stage in ['1','2','-2','3']: # this bill is in early stage, so cm must be one of the first meetings
+                bill.first_committee_meetings.add(cm)
+            else: # this bill is in later stages
+                v = bill.first_vote # look for first vote
+                if v and v.time.date() < cm.date:          # and check if the cm is after it,
+                    bill.second_committee_meetings.add(cm) # if so, this is a second committee meeting
+                else: # otherwise, assume its first cms.
+                    bill.first_committee_meetings.add(cm)
+            bill.update_stage()
+            action.send(request.user, verb='merged',
+                description=cm,
+                target=bill,
+                timestamp=datetime.datetime.now())
+
+        if user_input_type == 'mk':
+            mk_names = Member.objects.values_list('name',flat=True)
+            mk_name = difflib.get_close_matches(request.POST.get('mk_name'), mk_names)[0]
+            mk = Member.objects.get(name=mk_name)
+            cm.mks_attended.add(mk)
+            cm.save() # just to signal, so the attended Action gets created.
+            action.send(request.user, verb='merged',
+                description=cm,
+                target=mk,
+                timestamp=datetime.datetime.now())
+
         return HttpResponseRedirect(".")
 
 class MeetingsListView(ListView):
