a98ab63cf3c5d787345cbf62ea728346e2c8a63f
Benny Daon <bennydaon@gmail.com>
2010-08-21 10:43:58 +0300

    performance improvments

--- src/knesset/user/models.py
+++ src/knesset/user/models.py
@@ -37,7 +37,8 @@ class UserProfile(models.Model):
     def members(self):
         #TODO: ther has to be a faster way
         return map(lambda x: x.actor, 
-            Follow.objects.filter(user=self.user, content_type=ContentType.objects.get_for_model(Member)))
+            Follow.objects.filter(user=self.user, 
+                content_type=ContentType.objects.get_for_model(Member)).select_related('actor'))
 
     @property
     def parties(self):
