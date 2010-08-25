d36701e49d7530fda039dcbf433a22176373b3ac
ofri <ofri.raviv@gmail.com>
2010-08-26 02:08:45 +0300

    some cleanup in user views file

--- src/knesset/user/views.py
+++ src/knesset/user/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse, HttpResponseRedirect,HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden, \
+                        HttpResponseServerError, HttpResponseBadRequest, HttpResponseNotAllowed
 from django.contrib.auth import login, authenticate
 from forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 
 from datetime import datetime, timedelta
@@ -82,7 +82,7 @@ def create_user(request):
             user = authenticate(username=form.cleaned_data['username'], 
                                 password=form.cleaned_data['password1'])
             login(request, user)
-            ev = EmailValidation.objects.send(user=user)
+            EmailValidation.objects.send(user=user)
             next = request.POST.get('next', None)
             return HttpResponseRedirect(next if next else reverse('edit-profile'))
         else:
@@ -98,15 +98,14 @@ def edit_profile(request):
     if request.method == 'POST':
         edit_form = EditProfileForm(user=request.user, data=request.POST)
         if edit_form.is_valid():
-            user = edit_form.save()
+            edit_form.save()
             m = request.user.message_set.create()
             m.message = 'Your profile has been updated.'
             m.save()
             return HttpResponseRedirect('.')
 
     if request.method == 'GET':
-        edit_form = EditProfileForm(user = request.user)
-    payload = {'edit_form':edit_form}
+        edit_form = EditProfileForm(user = request.user)    
     return render_to_response('user/editprofile.html',
         context_instance=RequestContext(request,
             {'edit_form': edit_form,
@@ -115,7 +114,6 @@ def edit_profile(request):
 def follow_members(request):
     if not request.user.is_authenticated():
         return HttpResponseForbidden(reverse('login'))
-    p = request.user.get_profile()
     if request.method == 'POST':
         unwatch_id = request.POST.get('unwatch', None)
         if unwatch_id:
