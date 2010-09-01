5a180822415f25129d1a5393acb163db062e2840
ofri <ofri.raviv@gmail.com>
2010-09-01 12:04:36 +0300

    fixes to agenda edit/create forms

--- src/knesset/agendas/forms.py
+++ src/knesset/agendas/forms.py
@@ -7,10 +7,15 @@ from django.utils.translation import ugettext_lazy as _
 from models import Agenda
 
 class EditAgendaForm(forms.Form):
-    name = forms.CharField(max_length=300, label=_(u'Agenda name'), error_messages={'required': _('Please enter an agenda n'),
-                                                                                     'max_length': _('Your agenda title be shorter than 300 characters')})
-    description = forms.CharField(min_length=15, label=_(u'Agenda description'), error_messages={'required': _('Please enter a description for this agenda'),
-                                                                                                 'min_length': _('Your agenda description must be at least 15 characters long')})
+    name = forms.CharField(max_length=300, 
+                           label=_(u'Agenda name'), 
+                           error_messages={'required': _('Please enter an agenda name'),
+                                           'max_length': _('Agenda name must be shorter than 300 characters')})                                                           
+    description = forms.CharField(min_length=15, 
+                                  label=_(u'Agenda description'), 
+                                  error_messages={'required': _('Please enter a description for this agenda'),
+                                                  'min_length': _('Agenda description must be at least 15 characters long')},
+                                  widget=forms.Textarea)
     
     def __init__(self, agenda=None, *args, **kwargs):
         super(EditAgendaForm, self).__init__(*args, **kwargs)
@@ -21,6 +26,17 @@ class EditAgendaForm(forms.Form):
                             }
    
 class AddAgendaForm(ModelForm):
+    # to have the same names and help texts as the edit form, we need to override the form fields definitions:
+    name = forms.CharField(max_length=300, 
+                           label=_(u'Agenda name'), 
+                           error_messages={'required': _('Please enter an agenda name'),
+                                           'max_length': _('Agenda name must be shorter than 300 characters')})                                                           
+    description = forms.CharField(min_length=15, 
+                                  label=_(u'Agenda description'), 
+                                  error_messages={'required': _('Please enter a description for this agenda'),
+                                                  'min_length': _('Agenda description must be at least 15 characters long')},
+                                  widget=forms.Textarea)
+
     class Meta:
         model = Agenda
         fields = ('name', 'description')
