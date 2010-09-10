3c65cac3c8195fe87b1e6dbde0db5574faa63b41
ofri <ofri.raviv@gmail.com>
2010-09-10 22:32:11 +0300

    fixed cache on main page (don't cache at the template level, because it caches the logged in user...)

--- src/knesset/auxiliary/views.py
+++ src/knesset/auxiliary/views.py
@@ -2,6 +2,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.utils.translation import ugettext as _
 from django.conf import settings
+from django.core.cache import cache
 
 import random
 
@@ -10,15 +11,17 @@ from knesset.laws.models import Vote,Bill
 from tagging.models import Tag
 
 def main(request):
-    title = _('Home')
-    member = Member.objects.all()[random.randrange(Member.objects.count())]
-    votes = Vote.objects.filter_and_order(order='controversy')
-    vote = votes[random.randrange(votes.count())]
-    bill = Bill.objects.all()[random.randrange(Bill.objects.count())]
-    tags = Tag.objects.cloud_for_model(Bill)
-    tag = tags[random.randrange(len(tags))]
+    context = cache.get('main_page_context')
+    if not context:
+        context = {}
+        context['title'] = _('Home')
+        context['member'] = Member.objects.all()[random.randrange(Member.objects.count())]
+        votes = Vote.objects.filter_and_order(order='controversy')
+        context['vote'] = votes[random.randrange(votes.count())]
+        context['bill'] = Bill.objects.all()[random.randrange(Bill.objects.count())]
+        tags = Tag.objects.cloud_for_model(Bill)
+        context['tag'] = tags[random.randrange(len(tags))]
+        cache.set('main_page_context', context, 300) # 5 Minutes
     template_name = '%s.%s%s' % ('main', settings.LANGUAGE_CODE, '.html')    
-    return render_to_response(template_name,
-        {"title":title, "member":member, "vote":vote, "bill":bill, "tag":tag},
-        context_instance=RequestContext(request))
+    return render_to_response(template_name, context, context_instance=RequestContext(request))
 
