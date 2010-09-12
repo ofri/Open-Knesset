76f7287bad83a8e6c013ad4755ce224579fd4303
ofri <ofri.raviv@gmail.com>
2010-09-12 03:48:30 +0200

    lots of fixes in parsing code due to changes in beautifulSoup version.

--- src/knesset/simple/management/commands/mk_roles_parser.py
+++ src/knesset/simple/management/commands/mk_roles_parser.py
@@ -2,6 +2,8 @@
 # This Python file uses the following encoding: utf-8
 import urllib
 from BeautifulSoup import BeautifulSoup
+import logging
+logger = logging.getLogger("open-knesset.mk_roles_parser")
 
 GOVT_INFO_PAGE = r"http://www.knesset.gov.il/mk/heb/MKIndex_Current.asp?view=4";
 def parse_mk_govt_roles():
@@ -15,15 +17,19 @@ def parse_mk_govt_roles():
     return info_dict
 
 KNESSET_INFO_PAGE = r"http://www.knesset.gov.il/mk/heb/MKIndex_Current.asp?view=7";
-V1 = 'ועדת '
-V2 = 'ועדה '
+V1 = 'ועדת '.decode('utf8')
+V2 = 'ועדה '.decode('utf8')
 PREFIX = 'יו"ר '.decode('utf8')
 def parse_mk_knesset_roles():
     info_dict = {}
     soup = BeautifulSoup(urllib.urlopen(KNESSET_INFO_PAGE).read().decode('windows-1255').encode('utf-8'))
     tags = soup.findAll(lambda tag:tag.name=='tr' and tag.first().has_key('class') and ((tag.first()['class']=='MKIconM') or (tag.first()['class']=='MKIconF')))
     for tag in tags:
-        t = str(tag.find('a',{'style':"color:Black;"}).contents[0])
+        try:
+            t = tag.find('a',{'style':"color:Black;"}).contents[0]
+        except UnicodeEncodeError:
+            logger.warn('error parsing roles tag: %s' % tag.find('a',{'style':"color:Black;"}).contents[0])
+            continue
         if (t.find(V1)>=0) or (t.find(V2)>=0):    
             mk_id = tag.find('a').get('href').split('=')[1]
             mk_roles = PREFIX + tag.find('a',{'style':"color:Black;"}).contents[0]
