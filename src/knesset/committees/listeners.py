d2b9f52982d823605c3b828e509dd33fe6918390
Benny Daon <bennydaon@gmail.com>
2010-09-16 12:11:15 +0200

    adding a committee event on new annotation. next, display annotation on committee page

--- src/knesset/committees/listeners.py
+++ src/knesset/committees/listeners.py
@@ -25,6 +25,8 @@ post_save.connect(handle_cm_save, sender=CommitteeMeeting)
 @disable_for_loaddata
 def handle_annotation_save(sender, created, instance, **kwargs):
     if created:
+        action.send(instance.content_object.meeting.committe, verb='annotated',
+                    target=instance, description=unicode(instance.flag_value))
         action.send(instance.user, verb='annotated',
                     target=instance, description=unicode(instance.flag_value))
 post_save.connect(handle_annotation_save, sender=Annotation)
