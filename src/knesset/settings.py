90f1ee2e650c58c45fe62c10221189d2e44c544b
ofri <ofri.raviv@gmail.com>
2011-02-15 18:59:11 +0200

    updated deprecated settings to new ones

--- src/knesset/settings.py
+++ src/knesset/settings.py
@@ -14,12 +14,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'django.db.backends.sqlite3'         # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'dev.db'  # Or path to database file if using sqlite3.
-DATABASE_USER = ''      # Not used with sqlite3.
-DATABASE_PASSWORD = ''      # Not used with sqlite3.
-DATABASE_HOST = ''                  # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''                  # Set to empty string for default. Not used with sqlite3.
+DATABASES_ = {
+    'default': {
+        'NAME': 'dev.db',
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
+}
+DATABASE_ENGINE = 'django.db.backends.sqlite3' # Leaving this here for now for South compatibility.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -59,8 +60,8 @@ SECRET_KEY = '1_ovxxkf(c*z_dwv!(-=dezf#%l(po5%#zzi*su-$d*_j*1sr+'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",    
 #     'django.template.loaders.eggs.load_template_source',
 )
 
@@ -125,7 +126,7 @@ INSTALLED_APPS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-"django.core.context_processors.auth",
+"django.contrib.auth.context_processors.auth",
 "django.core.context_processors.debug",
 "django.core.context_processors.i18n",
 "django.core.context_processors.media",
