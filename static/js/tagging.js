1b927e8b4bb42e832961265c40a42bea1dfff9f3
ofri <ofri.raviv@gmail.com>
2011-03-24 17:08:34 +0200

    allow removing a just added tag

--- static/js/tagging.js
+++ static/js/tagging.js
@@ -45,10 +45,18 @@ function toggle_tag(tag_id) {
 function add_tag_callback(data) {    
     var item = eval('('+data+')');
     $('#no-tags-yet').remove();
-    if( $("#tag_"+item.id).length ){ return }; // we already have this tag.
-    var href = item.url;
-    $('<a class="tag">').attr("id", "tag_"+item.id).attr("href", href).html(item.name).appendTo("#tags");
-    $("#tags").append(document.createTextNode(" "));
+    if( $("#tag_"+item.id).length == 0 ){ // we don't already have this tag.
+        var href = item.url;
+        $('<a class="tag">').attr("id", "tag_"+item.id).attr("href", href).html(item.name).appendTo("#tags");
+        $("#tags").append(document.createTextNode(" "));
+    };
+    if( $("#possible_tag_"+item.id).length == 0 ){ // we don't already have this tag in the possible list.
+        href = "javascript:toggle_tag("+item.id+");";
+        $('<a class="tag">').attr("id", "possible_tag_"+item.id).attr("href", href).html(item.name).appendTo("#possible_tags");        
+        $("#possible_tags").append(document.createTextNode(" "));
+    };
+    $("#possible_tag_"+item.id).addClass("selected");
+    selected_tags[item.id]=1;
 }
 
 function remove_tag_callback(data) {
