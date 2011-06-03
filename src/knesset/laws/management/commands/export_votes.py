a1c54db294d2d3afd9385da5e54fe8931423840a
ofri <ofri.raviv@gmail.com>
2011-06-03 16:15:37 +0300

    added tags to votes export

--- src/knesset/laws/management/commands/export_votes.py
+++ src/knesset/laws/management/commands/export_votes.py
@@ -1,29 +1,39 @@
 import os, csv
+from operator import attrgetter
 from django.core.management.base import NoArgsCommand
 from django.conf import settings
 
+from tagging.models import Tag, TaggedItem
+
 from knesset.mks.models import Member,Party
-from knesset.laws.models import Vote,VoteAction
+from knesset.laws.models import Vote,VoteAction,Bill
 
 class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
         mks = Member.objects.order_by('current_party').values('id','name','current_party')
+        
+        vote_tags = Tag.objects.usage_for_model(Vote)
+        bill_tags = Tag.objects.usage_for_model(Bill)
+        all_tags = list(set(vote_tags).union(bill_tags))
+        all_tags.sort(key=attrgetter('name'))
+
         f = open(os.path.join(settings.DATA_ROOT, 'votes_mks.csv'), 'wt')
-        csv_writer = csv.writer(f)
-        header = []
-        header.append('Vote id')
-        header.append('Vote time')
-        header.append('Vote name')
+        mk_writer = csv.writer(f)
+        header = ['Vote id', 'Vote time', 'Vote name']
         for mk in mks:
             header.append(mk['id'])
-        csv_writer.writerow(header)
+        mk_writer.writerow(header)
 
-        for vote in Vote.objects.all():
-            row = []
-            row.append(vote.id)
-            row.append(vote.time)
-            row.append(vote.title.encode('utf8'))
+        f2 = open(os.path.join(settings.DATA_ROOT, 'votes_tags.csv'), 'wt')
+        tag_writer = csv.writer(f2)
+        header = ['Vote id', 'Vote time', 'Vote name']
+        for tag in all_tags:
+            header.append(tag.id)
+        tag_writer.writerow(header)
+
+        for vote in Vote.objects.iterator():
+            row = [vote.id, vote.time, vote.title.encode('utf8')]
             mks_for = vote.get_voters_id('for')
             mks_against = vote.get_voters_id('against')
             for mk in mks:
@@ -33,10 +43,21 @@ class Command(NoArgsCommand):
                     row.append(-1)
                 else:
                     row.append(0)
-            csv_writer.writerow(row)
+            mk_writer.writerow(row)
+
+            row = [vote.id, vote.time, vote.title.encode('utf8')]
+            tags = vote.tags
+            for tag in all_tags:
+                if tag in tags:
+                    row.append(1)
+                else:
+                    row.append(0)
+            tag_writer.writerow(row)
+
         f.close()
+        f2.close()
 
-
+        
 
 
 
