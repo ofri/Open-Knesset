c4036123146a184d247bba8de45666ad2d24f09c
Amit Zur <amitzur@zwr-zbys-macbook.local>
2011-06-03 19:25:08 +0300

    was-discussed-[12], some cleanups

--- src/knesset/laws/models.py
+++ src/knesset/laws/models.py
@@ -617,12 +617,12 @@ class Bill(models.Model):
                         timestamp=self.approval_vote.time, description=self.approval_vote.passed)
         
         for cm in self.first_committee_meetings.all():
-            action.send(self, verb='was-discussed', target=cm,
-                        timestamp=cm.date, description='1')
+            action.send(self, verb='was-discussed-1', target=cm,
+                        timestamp=cm.date, description=cm.committee.name)
             
         for cm in self.second_committee_meetings.all():
-            action.send(self, verb='was-discussed', target=cm,
-                        timestamp=cm.date, description='2')
+            action.send(self, verb='was-discussed-2', target=cm,
+                        timestamp=cm.date, description=cm.committee.name)
             
         for g in self.gov_decisions.all():
             action.send(self, verb='was-voted-on-gov', target=g,
