99c452d9e5e67557fdee186e158ea9b19faa137e
ofri <ofri.raviv@gmail.com>
2011-06-14 00:05:32 +0300

    added finding votes against one own's proposed bills. not presented to users yet

--- src/knesset/simple/management/commands/syncdata.py
+++ src/knesset/simple/management/commands/syncdata.py
@@ -87,7 +87,7 @@ class Command(NoArgsCommand):
                     exps.append('')
                     count += 1
             if re.search("""arrResume\[\d*\]""",line) != None:
-                r = re.search("""\"(.*)\"""",line)
+                r = re.search('"(.*)"',line)
                 if r != None:
                     try:
                         exps[count] += r.group(1).replace('\t',' ')
@@ -1491,6 +1491,10 @@ def update_vote_properties(v):
     opposition_stands_for = float(opposition_for_votes)>0.66*(opposition_for_votes+opposition_against_votes)
     opposition_stands_against = float(opposition_against_votes)>0.66*(opposition_for_votes+opposition_against_votes)
 
+    # a set of all MKs that proposed bills this vote is about.
+    proposers = [set(b.proposers.all()) for b in v.bills()]
+    if proposers:
+        proposers = reduce(lambda x,y: set.union(x,y), proposers)
 
     against_party_count = 0
     for va in VoteAction.objects.filter(vote=v):
@@ -1510,6 +1514,11 @@ def update_vote_properties(v):
             if (opposition_stands_for and va.type=='against') or (opposition_stands_against and va.type=='for'):
                 va.against_opposition = True
                 dirt = True
+
+        if va.member in proposers and va.type=='against':
+            va.against_own_bill = True
+            dirt = True
+
         if dirt:
             va.save()
 
