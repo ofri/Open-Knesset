3e4807f69ab48571dcb8c0b3efeeec6da8599860
Meir Kriheli <mkriheli@gmail.com>
2011-07-04 21:20:13 +0300

    Remove hashnav.dates

--- src/knesset/hashnav/__init__.py
+++ src/knesset/hashnav/__init__.py
@@ -2,9 +2,6 @@ from functools import wraps, update_wrapper
 from base import View
 from list import ListView
 from detail import DetailView
-from dates import (ArchiveView, YearView, MonthView,
-                                     WeekView, DayView, TodayView,
-                                     DateDetailView)
 from edit import (FormView, CreateView, UpdateView,
                                     DeleteView)
 def method_decorator(decorator):
