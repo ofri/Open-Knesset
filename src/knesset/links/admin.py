488298423fcb6b9ebe4757c0a0c49e8efcdff8fe
David Shimon <david206@gmail.com>
2011-07-06 01:04:40 +0300

    add management command to deactivate duplicate links. (compared by url and object content), Task #2155. add actions to links admin for activation and deactivation of links.

--- src/knesset/links/admin.py
+++ src/knesset/links/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from models import *
 from django.utils.translation import ugettext_lazy as _, ungettext
 
+def make_active(modeladmin, request, queryset):
+    queryset.update(active=True)
+make_active.short_description = "Mark selected links as active"
+
+def make_inactive(modeladmin, request, queryset):
+    queryset.update(active=False)
+make_inactive.short_description = "Mark selected links as inactive"
+
 class LinkTypesAdmin(admin.ModelAdmin):
     pass
 
@@ -18,6 +26,7 @@ class LinksAdmin(admin.ModelAdmin):
     list_display = ('title', 'link_type', 'content_type', 'object_pk')
     search_fields = ('title',)
     list_filter = ('link_type','active' )
+    actions = [make_active, make_inactive ]
 
 
 admin.site.register(Link, LinksAdmin)
