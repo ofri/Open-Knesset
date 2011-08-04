b047526828fce6233f3ea9c4486f0751ff5045a5
David Shimon <david206@gmail.com>
2011-08-05 01:44:09 +0300

    seperate bill item list template and reuse it on both bill list and bill list by tag

--- src/knesset/laws/templatetags/bills_extra.py
+++ src/knesset/laws/templatetags/bills_extra.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.inclusion_tag('laws/bill_full_name.html')
 def bill_full_name(bill):
     return { 'bill': bill }
+
+@register.inclusion_tag('laws/bill_list_item.html')
+def bill_list_item(bill):
+    return { 'bill': bill }
