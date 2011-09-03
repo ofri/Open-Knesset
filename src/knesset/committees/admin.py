9dd140054709f52a850ff0facca4e9b49dc87d19
daonb <bennydaon@gmail.com>
2011-09-03 16:21:40 +0300

    many small improvments in commiteee topics

--- src/knesset/committees/admin.py
+++ src/knesset/committees/admin.py
@@ -1,3 +1,4 @@
+from django.contrib.contenttypes.generic import GenericTabularInline
 from knesset.committees.models import *
 
 from django.contrib import admin
@@ -10,6 +11,21 @@ class CommitteeMeetingAdmin(admin.ModelAdmin):
     ordering = ('-date',)
 admin.site.register(CommitteeMeeting, CommitteeMeetingAdmin)
 
+class LinksTable(GenericTabularInline):
+    model = Link
+    ct_field='content_type'
+    ct_fk_field='object_pk'
+
+class EventsTable(GenericTabularInline):
+    model = Event
+    ct_field='which_type'
+    ct_fk_field='which_pk'
+
 class TopicAdmin(admin.ModelAdmin):
     ordering = ('-created',)
+    inlines = [
+        LinksTable,
+        EventsTable,
+    ]
+
 admin.site.register(Topic, TopicAdmin)
--- src/knesset/topics/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import ListView
-from models import Topic
-
-class TopicListView(ListView):
-    model = Topic
-    context_object_name = 'topics'
-
