b8ff8e4b341b4f964f528d966cd85fe0e3e3e9f4
daonb <bennydaon@gmail.com>
2011-10-04 00:00:09 +0200

    adding the update_nks_users management command

---
+++ src/knesset/mks/management/commands/update_mks_users.py
@@ -0,0 +1,49 @@
+import urllib2
+
+from django.core.management.base import NoArgsCommand
+from django.core.files.base import ContentFile
+from django.contrib.auth.models import User, Group
+
+from knesset.mks.models import Member
+from avatar.models import Avatar
+
+
+class Command(NoArgsCommand):
+    help = "Copy information from Member model to User, UserPrifle and Avatar"
+
+    def handle_noargs(self, **options):
+        # TODO: VALID_EMAIL_GROUP should be project-wide 
+        valid_email = Group.objects.get(name='Valid Email')
+        for member in Member.objects.all():
+            self.stdout.write(u"filling user data for member #%d (%s)\n" % ( member.id, member.name ))
+            user = member.user
+            if not user: user = User()
+            user.email = member.email
+            user.username = member.email.split('@')[0]
+
+            names = member.name.split(' ')
+            user.first_name = ' '.join(names[:-1])
+            if user.first_name:
+                user.last_name = names[-1]
+            else:
+                # just one name, better make it the first
+                user.first_name = names[0]
+                user.last_name = ''
+
+            user.save()
+            user.groups.add(valid_email)
+            avatars =  Avatar.objects.filter(user=user)
+            avatar = avatars[0] if avatars else user.avatar_set.create()
+
+            image_url = urllib2.urlopen(member.img_url)
+            content = ContentFile(image_url.read())
+            avatar.avatar.save(user.username, content)
+            profile = user.get_profile()
+            profile.email_notification='D'
+            profile.description = member.get_role
+            profile.gender = member.gender
+            profile.save()
+
+            if not member.user:
+                member.user = user
+                member.save()
