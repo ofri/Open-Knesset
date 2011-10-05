1a0f4f284d246d39a3c2cf5539ff0fdda012ec9f
ofri <ofri.raviv@gmail.com>
2011-10-06 01:12:54 +0200

    remove debug prints. should stop server from crashing when running under mod_wsgi

--- src/knesset/agendas/forms.py
+++ src/knesset/agendas/forms.py
@@ -14,20 +14,20 @@ class H4(forms.Widget):
         return mark_safe(u'<h4>%s</h4>' % value)
 
 class EditAgendaForm(forms.Form):
-    name = forms.CharField(max_length=300, 
-                           label=_(u'Agenda name'), 
+    name = forms.CharField(max_length=300,
+                           label=_(u'Agenda name'),
                            error_messages={'required': _('Please enter an agenda name'),
-                                           'max_length': _('Agenda name must be shorter than 300 characters')})                                                           
+                                           'max_length': _('Agenda name must be shorter than 300 characters')})
     public_owner_name = forms.CharField(max_length=100,
                                         label=_(u'Public owner name'),
                                         error_messages={'required': _('Please enter a public owner name'),
                                                         'max_length': _('Public owner name must be shorter than 100 characters')})
-    description = forms.CharField(min_length=15, 
-                                  label=_(u'Agenda description'), 
+    description = forms.CharField(min_length=15,
+                                  label=_(u'Agenda description'),
                                   error_messages={'required': _('Please enter a description for this agenda'),
                                                   'min_length': _('Agenda description must be at least 15 characters long')},
                                   widget=forms.Textarea)
-    
+
     def __init__(self, agenda=None, *args, **kwargs):
         super(EditAgendaForm, self).__init__(*args, **kwargs)
         self.agenda = agenda
@@ -36,19 +36,19 @@ class EditAgendaForm(forms.Form):
                             'public_owner_name': self.agenda.public_owner_name,
                             'description': self.agenda.description,
                             }
-   
+
 class AddAgendaForm(ModelForm):
     # to have the same names and help texts as the edit form, we need to override the form fields definitions:
-    name = forms.CharField(max_length=300, 
-                           label=_(u'Agenda name'), 
+    name = forms.CharField(max_length=300,
+                           label=_(u'Agenda name'),
                            error_messages={'required': _('Please enter an agenda name'),
                                            'max_length': _('Agenda name must be shorter than 300 characters')})
     public_owner_name = forms.CharField(max_length=100,
                                         label=_(u'Public owner name'),
                                         error_messages={'required': _('Please enter a public owner name'),
                                                         'max_length': _('Public owner name must be shorter than 100 characters')})
-    description = forms.CharField(min_length=15, 
-                                  label=_(u'Agenda description'), 
+    description = forms.CharField(min_length=15,
+                                  label=_(u'Agenda description'),
                                   error_messages={'required': _('Please enter a description for this agenda'),
                                                   'min_length': _('Agenda description must be at least 15 characters long')},
                                   widget=forms.Textarea)
@@ -64,15 +64,13 @@ class VoteLinkingForm(forms.Form):
     agenda_id = forms.IntegerField(widget=forms.HiddenInput) #TODO: hide this!
     weight = forms.TypedChoiceField(label=_('Position'), choices=AGENDAVOTE_SCORE_CHOICES,
              required=False, widget=forms.RadioSelect)
-    reasoning = forms.CharField(required=False, max_length=300, 
-                           label=_(u'Reasoning'), 
+    reasoning = forms.CharField(required=False, max_length=300,
+                           label=_(u'Reasoning'),
                            widget = forms.Textarea(attrs={'cols':30, 'rows':5}),
                            )
-    
+
     def clean_weight(self):
-        print "clean weight"
         data = self.cleaned_data['weight']
-        print data
         if data=="":
             return 99
         return data
@@ -82,6 +80,6 @@ class VoteLinkingForm(forms.Form):
         if cleaned_data.get('weight') == 99:
             cleaned_data["DELETE"] = 'on'
         return cleaned_data
-        
+
 VoteLinkingFormSet = formset_factory(VoteLinkingForm, extra=0, can_delete=True)
 
