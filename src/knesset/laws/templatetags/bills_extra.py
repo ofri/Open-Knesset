23780826115f4acab955124f9d4c2a1e50327a7d
gardenofwine <gardenofwine@gmail.com>
2011-12-11 10:05:39 +0200

    Bill Embed - finish up pre-vote (before code review)

--- src/knesset/laws/templatetags/bills_extra.py
+++ src/knesset/laws/templatetags/bills_extra.py
@@ -1,3 +1,4 @@
+#encoding: utf-8
 from django import template
 
 register = template.Library()
@@ -16,4 +17,31 @@ def item_tags(tags):
 
 @register.inclusion_tag('laws/bill_inabox.html')
 def bill_inabox(bill):
-    return { 'bill': bill }
+    """ TODO: firstX and not first3"""
+
+#    CONVERT_TO_DISCUSSION_HEADERS = ('להעביר את הנושא'.decode('utf8'), 'העברת הנושא'.decode('utf8'))
+
+#    discussion = False
+#    for v in bill.pre_votes.all():
+#        for h in CONVERT_TO_DISCUSSION_HEADERS:
+#            if v.title.find(h)>=0: # converted to discussion
+#                discussion = True
+    pre_vote_count = bill.pre_votes.count()
+    pre_vote_against_width = 0
+    pre_vote_fields = {}
+    if pre_vote_count > 0:
+        pre_vote_last = bill.pre_votes.all()[pre_vote_count - 1]
+        pre_vote_against_width = (pre_vote_last.against_votes_count() * 120) / (pre_vote_last.for_votes_count() + pre_vote_last.against_votes_count())
+        pre_vote_fields = { 'pre_vote_against_width' : pre_vote_against_width,
+                       'pre_vote_backgroundPosition' : pre_vote_against_width - 120,
+                        # what is the real index? 0 is not the correct one
+                        'pre_vote_passed' : bill.pre_votes.all()[0].for_votes_count() > bill.pre_votes.all()[0].against_votes_count(),
+                        'pre_vote_date' : "%s/%s<br>%s" % (bill.pre_votes.all()[0].time.day, bill.pre_votes.all()[0].time.month, bill.pre_votes.all()[0].time.year)}
+
+    return dict({ 'bill': bill,
+            'proposers_first3' : bill.proposers.all()[:3],
+            'proposers_count_minus3' : bill.proposers.count() - 3,
+#            'was_converted_to_discussion' : discussion,
+            'was_pre_voted' : pre_vote_count > 0,
+            'pre_vote_passed' : "",
+            'pre_vote_date' : "?"}.items() + pre_vote_fields.items())
