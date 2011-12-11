16cfd7c3667d651d006f7ea5ba98315cb3f9a17c
gardenofwine <gardenofwine@gmail.com>
2011-12-11 13:20:25 +0200

    Bill Embed - refactor by moving all styling related code to the the html file template (via javascipt)

--- src/knesset/laws/templatetags/bills_extra.py
+++ src/knesset/laws/templatetags/bills_extra.py
@@ -1,5 +1,6 @@
 #encoding: utf-8
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -26,22 +27,12 @@ def bill_inabox(bill):
 #        for h in CONVERT_TO_DISCUSSION_HEADERS:
 #            if v.title.find(h)>=0: # converted to discussion
 #                discussion = True
-    pre_vote_count = bill.pre_votes.count()
-    pre_vote_against_width = 0
-    pre_vote_fields = {}
-    if pre_vote_count > 0:
-        pre_vote_last = bill.pre_votes.all()[pre_vote_count - 1]
-        pre_vote_against_width = (pre_vote_last.against_votes_count() * 120) / (pre_vote_last.for_votes_count() + pre_vote_last.against_votes_count())
-        pre_vote_fields = { 'pre_vote_against_width' : pre_vote_against_width,
-                       'pre_vote_backgroundPosition' : pre_vote_against_width - 120,
-                        # what is the real index? 0 is not the correct one
-                        'pre_vote_passed' : bill.pre_votes.all()[0].for_votes_count() > bill.pre_votes.all()[0].against_votes_count(),
-                        'pre_vote_date' : "%s/%s<br>%s" % (bill.pre_votes.all()[0].time.day, bill.pre_votes.all()[0].time.month, bill.pre_votes.all()[0].time.year)}
 
-    return dict({ 'bill': bill,
+    # what is the real index? 0 is not the correct one
+    return dict({ 'MEDIA_URL' : settings.MEDIA_URL,
+                 'bill': bill,
             'proposers_first3' : bill.proposers.all()[:3],
             'proposers_count_minus3' : bill.proposers.count() - 3,
-#            'was_converted_to_discussion' : discussion,
-            'was_pre_voted' : pre_vote_count > 0,
-            'pre_vote_passed' : "",
-            'pre_vote_date' : "?"}.items() + pre_vote_fields.items())
+            'pre_vote_time' : {'day' : bill.pre_votes.all()[0].time.day,
+                               'month' : bill.pre_votes.all()[0].time.month,
+                               'year' : bill.pre_votes.all()[0].time.year}})
