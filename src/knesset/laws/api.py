d0934fca15c85940dea7f9451462418ef1310333
daonb <bennydaon@gmail.com>
2012-04-29 12:50:59 +0300

    better bill api, not there yet

--- src/knesset/laws/api.py
+++ src/knesset/laws/api.py
@@ -11,11 +11,16 @@ from video.utils import get_videos_queryset
 from video.api import VideoResource
 from links.models import Link
 from links.api import LinkResource
-from models import Law, Bill
+from models import Law, Bill, Vote
 
 class LawResource(BaseResource):
     class Meta:
         queryset = Law.objects.all()
+        allowed_methods = ['get']
+
+class VoteResource(BaseResource):
+    class Meta:
+        queryset = Vote.objects.all()
         allowed_methods = ['get']
 
 class BillResource(BaseResource):
@@ -23,17 +28,45 @@ class BillResource(BaseResource):
     class Meta:
         queryset = Bill.objects.all()
         allowed_methods = ['get']
-        ordering = [
-            'title',
-            'stage',
-        ]
-        filtering = dict(
-            stage = ALL,
-            )
+        # excludes = ['stage']
+        ordering = ['title', 'stage']
+        filtering = dict(stage = ALL)
+
     proposers = fields.ToManyField(MemberResource,
                     'proposers',
-                    full=True
-                )
+                    full=True)
+    pre_votes = fields.ToManyField(VoteResource,
+                    'pre_votes',
+                    null=True,
+                    full=True)
+
+    first_vote = fields.ToOneField(VoteResource,
+                    'first_vote',
+                    null=True,
+                    full=True)
+
+    approval_vote = fields.ToOneField(VoteResource,
+                    'approval_vote',
+                    null=True,
+                    full=True)
+
 
     law = fields.ToOneField(LawResource, 'law', null=True, full=True)
 
+    def dehydrate_stage(self, bundle):
+        return bundle.obj.get_stage_display()
+
+    def dehydrate(self, bundle):
+        from simple.management.commands.syncdata import p_explanation
+        bill = bundle.obj
+        ps = bill.proposals.order_by('-date')[0]
+        if ps.content_html:
+            parts = ps.content_html.split(p_explanation)
+            try:
+                bundle.data.update(dict(legal_code = parts[0]+'</p>',
+                    explanation = '<p>' + parts[1]))
+            except IndexError:
+                pass
+
+        # bundle.data['stage'] = bill.get_stage_display()
+        return bundle
