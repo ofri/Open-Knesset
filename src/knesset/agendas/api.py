e7fcdc751c85800ab3c896cf57d226f0aae755ee
daonb <bennydaon@gmail.com>
2012-04-30 20:13:41 +0300

    adding agenda editors

--- src/knesset/agendas/api.py
+++ src/knesset/agendas/api.py
@@ -1,12 +1,27 @@
 '''
 API for the agendas app
 '''
+from django.contrib.auth.models import User
 from tastypie.constants import ALL
 import tastypie.fields as fields
+from avatar.templatetags.avatar_tags import avatar_url
 
 from knesset.api.resources.base import BaseResource
 from mks.models import Member, Party
 from agendas.models import Agenda, AgendaVote
+
+class UserResource(BaseResource):
+    class Meta(BaseResource.Meta):
+        queryset = User.objects.all()
+        include_absolute_url = True
+        include_resource_uri = False
+        allowed_methods = ['get']
+        fields = ['username']
+
+    avatar = fields.CharField()
+
+    def dehydrate_avatar(self, bundle):
+        return avatar_url(bundle.obj, 48)
 
 class AgendaVoteResource(BaseResource):
     class Meta(BaseResource.Meta):
@@ -29,9 +44,13 @@ class AgendaResource(BaseResource):
         excludes = ['is_public']
 
     followers = fields.IntegerField(attribute="number_of_followers")
+    editors = fields.ToManyField(UserResource,
+                    'editors',
+                    full=True)
     votes = fields.ToManyField(AgendaVoteResource,
                     'agendavotes',
                     full=True)
+
     def dehydrate(self, bundle):
         a = bundle.obj
         bundle.data['members'] = map(
