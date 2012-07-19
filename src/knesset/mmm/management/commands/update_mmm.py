8bf2fe8bc160e7c6b020428e4d401ecc394bdca8
ana <alkadur@gmail.com>
2012-07-19 12:28:19 +0200

    Small improvements to update_mmm.py command

--- src/knesset/mmm/management/commands/update_mmm.py
+++ src/knesset/mmm/management/commands/update_mmm.py
@@ -1,4 +1,4 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import NoArgsCommand, CommandError
 from knesset.mmm.models import Document
 import simplejson
 
@@ -8,6 +8,10 @@ class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
         
-        json = open(DATA_ROOT + 'mmm_matches.json')
+        try:
+            url = DATA_ROOT + 'mmm_matches.json'
+            json = open(url, 'r')
+        except IOError as e:
+            raise CommandError('[Errno - {0}]: {1} {2}'.format(e.errno, e.strerror, url))
         
         Document.objects.from_json(json)
