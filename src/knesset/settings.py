318f6c32aa3e78fb1069dffbf5c5df94734cf856
ofri <ofri.raviv@gmail.com>
2012-07-26 21:17:16 +0300

    add line number to logging

--- src/knesset/settings.py
+++ src/knesset/settings.py
@@ -191,7 +191,7 @@ logger = logging.getLogger("open-knesset")
 logger.setLevel(logging.DEBUG)  # override this in prod server to logging.ERROR
 h = logging.FileHandler(LOG_FILENAME)
 h.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s")
+formatter = logging.Formatter("%(asctime)s\t%(name)s:%(lineno)d\t%(levelname)s\t%(message)s")
 h.setFormatter(formatter)
 logger.addHandler(h)
 
