faa484266a2b9b30c648d1df1ded864235fe6121
ofri <ofri.raviv@gmail.com>
2012-10-13 00:58:09 +0200

    add notification to email when meeting is added/updated/removed in agenda

--- src/knesset/agendas/listeners.py
+++ src/knesset/agendas/listeners.py
@@ -6,7 +6,7 @@ from planet.models import Feed, Post
 from actstream import action
 from actstream.models import Follow
 from knesset.utils import cannonize, disable_for_loaddata
-from knesset.agendas.models import AgendaVote
+from knesset.agendas.models import AgendaVote, AgendaMeeting
 from knesset.agendas.models import Agenda
 from knesset.links.models import Link, LinkType
 
@@ -24,7 +24,6 @@ def record_agenda_ascription_action(sender, created, instance, **kwargs):
                     timestamp = datetime.datetime.now())
 post_save.connect(record_agenda_ascription_action, sender=AgendaVote)
 
-
 @disable_for_loaddata
 def record_agenda_removal_action(sender, instance, **kwargs):
     action.send(instance.agenda, verb='agenda removed',
@@ -32,6 +31,32 @@ def record_agenda_removal_action(sender, instance, **kwargs):
                 target = instance.vote,
                 timestamp = datetime.datetime.now())
 pre_delete.connect(record_agenda_removal_action, sender=AgendaVote)
+
+@disable_for_loaddata
+def record_agenda_meeting_ascription_action(sender, created, instance, **kwargs):
+    if created:
+        action.send(instance.agenda, verb='agenda_meeting_ascribed',
+                    description='agenda "%s" ascribed to meeting "%s"' %
+                    (instance.agenda.__unicode__(),instance.meeting.title()),
+                    target = instance,
+                    timestamp = datetime.datetime.now())
+    else:
+        action.send(instance.agenda, verb='agenda_meeting_relation_updated',
+                    description='relation between agenda "%s" and meeting "%s" was updated' %
+                    (instance.agenda.__unicode__(),
+                     instance.meeting.title()),
+                    target = instance,
+                    timestamp = datetime.datetime.now())
+post_save.connect(record_agenda_meeting_ascription_action, sender=AgendaMeeting)
+
+@disable_for_loaddata
+def record_agenda_meeting_removal_action(sender, instance, **kwargs):
+    action.send(instance.agenda, verb='agenda_meeting_removed',
+                description='agenda "%s" removed from meeting "%s"' %
+                (instance.agenda.__unicode__(),instance.meeting.title()),
+                target = instance.meeting,
+                timestamp = datetime.datetime.now())
+pre_delete.connect(record_agenda_meeting_removal_action, sender=AgendaMeeting)
 
 @disable_for_loaddata
 def update_num_followers(sender, instance, **kwargs):
