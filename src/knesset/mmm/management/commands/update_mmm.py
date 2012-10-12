709d5254bfdd6b2234263032089eb62f0e4973b6
Ana <ana@hp.(none)>
2012-10-12 19:55:31 +0200

    Refactored update_mmm.

--- src/knesset/mmm/management/commands/update_mmm.py
+++ src/knesset/mmm/management/commands/update_mmm.py
@@ -7,32 +7,44 @@ from knesset.mmm.models import Document
 from knesset.settings import DATA_ROOT
 
 
-def parse_json(fp):
-    """ receives fp from data folder, loads/parses and returns a list of dictionaries """
+def parse_json(str):
+    """ receives fp.read result, loads/parses and returns a list of dictionaries """
 
-    result = simplejson.load(fp)
+    result = simplejson.loads(str)
 
-    # modifying the data to be suitable for use
+    # translating date strings to datetime objects
     for o in result:
-    #        o['candidates'] = re.sub(r"\s+", r" " , " ".join(o['candidates']))
         o['date'] = datetime.strptime(o['date'], '%d/%m/%Y')
 
     return result
 
+def combine_jsons(matches, mmm):
+    """
+    params:
+      matches - fp.read() result of matches.json
+      mmm - fp.read() result of mmm.json
+
+    returns:
+      json - list of matches objects with added authors from mmm.
+    """
+    # parse matches.json and mmm.json to python obj's
+    json = parse_json(matches)
+    mmm = simplejson.loads(mmm)
+
+    # create a dictionary of urls - authors
+    authors = dict((o['url'], ', '.join(o['authors'])) for o in mmm)
+
+    # modifying matches to include author field
+    for i in json:
+        i['author'] = authors[i['url']]
+
+    return json
+
 class Command(NoArgsCommand):
     help = "Updating mmm table"
 
+
     def handle_noargs(self, **options):
-
-        # parse matches.json and mmm.json to python obj's
-        json = parse_json(open(DATA_ROOT + 'matches.json'))
-        mmm = parse_json(open(DATA_ROOT + 'mmm.json'))
-
-        # create a dictionary of urls - authors
-        authors = dict((o['url'], ', '.join(o['authors'])) for o in mmm)
-
-        # modifying matches to include author field
-        for i in json:
-            i['author'] = authors[i['url']]
-
-        Document.objects.from_json(json)
+        json1 = open(DATA_ROOT + 'matches.json', 'rt').read()
+        json2 = open(DATA_ROOT + 'mmm.json', 'rt').read()
+        Document.objects.from_json(combine_jsons(json1, json2))
