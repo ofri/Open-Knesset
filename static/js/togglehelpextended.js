1c0b49c3fe0e4d50e39f5c7a1e827f752f1059a6
Adaya <adaya.cohen@gmail.com>
2012-10-13 11:43:15 +0200

    Modify the help section template

--- static/js/togglehelpextended.js
+++ static/js/togglehelpextended.js
@@ -1,8 +1,16 @@
 function toggleHelpExtended() {
 	$("#help-extended").toggle();
+	$("#toggle-help").hide();
 };
+
+function unToggleHelpExtended() {
+	$("#help-extended").toggle();
+	$("#toggle-help").show();
+};
+
 
 $(document).ready(function() {
 	$("#toggle-help").click(function() {toggleHelpExtended();});
+	$("#un-toggle-help").click(function() {unToggleHelpExtended();});
 	$("#help-extended").hide();
 		});
