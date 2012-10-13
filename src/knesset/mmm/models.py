e4adf0c7ba3a2e7a0d9bd735d3cc4131f682fee5
neara <alkadur@gmail.com>
2012-10-13 14:53:42 +0200

    Added better fuzzy matching

--- src/knesset/mmm/models.py
+++ src/knesset/mmm/models.py
@@ -1,26 +1,25 @@
 import logging
-import difflib
 
 from django.db import models
 from django.utils import simplejson
 
 from knesset.mks.models import Member
 from knesset.committees.models import Committee
+from fuzzy_match import fuzzy_match
 
 
 logger = logging.getLogger("open-knesset.mmm.models")
 
-def text_lookup(modelName, text):
-    """receives a text and a modelName and returns a list of modelName objects found in the text"""
+def text_lookup(Model, text):
+    """receives a text and a Model and returns a list of Model objects found in the text"""
     
     result = []
 
-    for m in modelName.objects.all():
+    for m in Model.objects.all():
         if m.name in text:
             result.append(m.id)
         else:
-            k = difflib.SequenceMatcher(None, m.name, text)
-            if k.ratio() >= 0.6:
+            if fuzzy_match(m.name, text):
                 logger.warning('No exact match found. Performing fuzzy matching!')
                 result.append(m.id)
     
@@ -31,10 +30,10 @@ def verify(o, i, mks, committees):
     if i[0].title == o['title'] and i[0].publication_date == o['date'] and i[0].author_names == o['author']:
         if i[0].req_mks == mks or i[0].req_committees == committees:
             logger.info("%s already exists in db" % o['url'])
-            return True
-        else:
-            logger.warning("Failed DB verification! Encountered conflicts between the object's mks and committees in our db and imported data.")
             return False
+        else:
+            logger.info("Found differences between imported data and our db. Performing Update!")
+            return True
     else:
         logger.warning("Failed DB verification! Encountered multiple conflicts between the object in our db and imported data.")
         return False
@@ -52,13 +51,15 @@ class DocumentManager(models.Manager):
             committees = text_lookup(Committee, o['heading'])
             
             # db verification
-            if i.exists():
+            if i:
                 if len(i) != 1:
                     logger.warning("Corrupted DB! More than one object were found with same url.")
                 else:
-                    verify(o, i, mks, committees)
+                   if verify(o, i, mks, committees):
+                       i.req_mks = mks
+                       i.req_committee = committees
             else:
-                logger.info("creating new Document instance: %s" % o['url'])
+                logger.info("Creating new Document instance: %s" % o['url'])
                 d = self.create(url=o['url'], title=o['title'], publication_date=o['date'], author_names=o['author'])
                 d.req_committee = committees
                 d.req_mks = mks
