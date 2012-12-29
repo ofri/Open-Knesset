ec11e2c93295be265fcbaee0644053197b4bdd80
orifolger <ori.folger@gmail.com>
2012-12-29 13:10:04 +0200

    Add a management command for importing candidate lists and candidates from CSV files

--- polyorg/management/commands/import_candidatelist.py
+++ polyorg/management/commands/import_candidatelist.py
@@ -1,28 +1,40 @@
 # encoding: utf-8
-import logging, csv
+import logging, csv, itertools
 from collections import namedtuple
 from django.core.management.base import BaseCommand
+from polyorg.models import Candidate
+from polyorg.models import CandidateList
+from mks.models import Member
+from persons.models import Person
+from django.db import transaction
 
 logger = logging.getLogger("open-knesset.polyorg.import_candidatelist")
 
-CandidateTuple = namedtuple('CandidateTuple', ['comments', 'mqg', 'manifest_url', 'party_website', 'image_url', 'id','ordinal','member','letters','party'])
+CandidateTuple = namedtuple('CandidateTuple', ['comments', 'mqg', 'manifest_url', 'party_website', 'image_url', 'mk_id', 'ordinal', 'name', 'ballot', 'list'])
 
 class Command(BaseCommand):
+    def _list_by_cnd(self, cnd):
+        ls = CandidateList.objects.filter(name__exact = cnd.list)
+        if ls:
+            return ls[0]
+        clist = CandidateList.objects.create(name = cnd.list, ballot = cnd.ballot)
+        return clist
+
+    def _person_by_cnd(self, cnd):
+        persons = list(Person.objects.filter(name__exact = cnd.name))
+        if cnd.mk_id:
+            persons = list(Person.objects.filter(mk__id = cnd.mk_id)) + persons
+        if persons:
+            return persons[0]
+        return Person.objects.create(name = cnd.name)
+
+    @transaction.commit_on_success()
     def handle(self, *args, **options):
+        # For each candidate:
+        #    Build a Candidate object
         for csv_file in args:
             self.stdout.write('Import from %s\n' % (csv_file))
-            #logger.debug('Import from %s' % (csv_file))
-            #with open(csv_file, 'rb') as cf:
-            #    reader = csv.reader(cf) #, delimiter=' ', quotechar='|')
-            idx = 0
-            for cnd in map(CandidateTuple._make, csv.reader(open(csv_file, "rb"))):
-                if idx > 0:
-                    # a record, not list of columns
-                    if cnd.id:
-                        print '%s has ID %s' % (cnd.member, cnd.id)
-                        
-                    else:
-                        print '%s has no ID' % (cnd.member)
-                idx += 1
-                if idx > 12:
-                    break
+            for cnd in itertools.islice(map(CandidateTuple._make, csv.reader(open(csv_file, "rb"))), 2, None):
+                clist = self._list_by_cnd(cnd)
+                person = self._person_by_cnd(cnd)
+                candidate = Candidate.objects.create(candidates_list = clist, person = person, ordinal = cnd.ordinal)
