d57c67243c4541eb184a99faae19231b1f23a89c
Meir Kriheli <mkriheli@gmail.com>
2013-01-03 17:33:06 +0200

    OKMore bootstrap plugin

---
+++ static/js/ok-more.js
@@ -0,0 +1,88 @@
+!function ($) {
+
+"use strict"; // jshint ;_;
+
+	var OKMore = function (element, options, source) {
+		this.$element = $(element)
+		this.$source = $(source)
+		this.options = $.extend({}, $.fn.okmore.defaults, options)
+
+    this.initial = parseInt(this.options.initial);
+    this.page = parseInt(this.options.page);
+    this.total = parseInt(this.options.total);
+	}
+
+	OKMore.prototype = {
+		constructor: OKMore
+
+		, getMore: function() {
+        var that = this;
+
+        this.$source.button('loading');
+        
+        $.ajax({
+          url: this.options.url,
+          data:{initial: this.initial, page:this.page+1},
+          context:this
+        }).done(function(data) {
+          $(data.content).appendTo(this.$element);
+          this.page = data.current;
+          this.total = data.total;
+          this.initial = null;
+
+          if (data.has_next) {
+            this.$source.button('reset');
+          }
+          else {this.$source.remove()}
+        });
+
+		}
+	}
+
+  var old = $.fn.okmore
+
+  $.fn.okmore = function (source) {
+    var $source = $(source)
+      , source_options = $source.data();
+
+    source_options.url = source_options.url || $source.attr("href");
+
+    return this.each(function () {
+      var $this = $(this)
+        , data = $this.data('okmore')
+        , options = source_options;
+
+      if (!data) $this.data('okmore', (data = new OKMore(this, options, source)))
+
+      data.getMore()
+    })
+  }
+
+  $.fn.okmore.defaults = {
+    page: 0,
+    initial: 2,
+    total: 999999
+  }
+
+  $.fn.okmore.Constructor = OKMore
+
+
+ /* okmore NO CONFLICT
+  * ==================== */
+
+  $.fn.okmore.noConflict = function () {
+    $.fn.okmore = old
+    return this
+  }
+
+
+ /* okmore DATA-API
+  * ================= */
+  $(document).on('click.okmore.data-api', '[data-provide=okmore]', function (e) {
+      
+      var $this = $(this)
+        , target = $this.attr('data-target');
+      e.preventDefault();
+      $(target).okmore(this)
+  })
+}(window.jQuery);
