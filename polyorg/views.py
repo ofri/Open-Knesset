8d7293c44a5647191b455566934fb40b368881cf
Benny Daon <bennydaon@gmail.com>
2013-01-15 17:26:03 +0200

    adding caching

--- polyorg/views.py
+++ polyorg/views.py
@@ -1,4 +1,6 @@
 import json
+from django.conf import settings
+from django.core.cache import cache
 from django.views.generic import ListView, TemplateView
 from django.utils.translation import ugettext as _
 from hashnav.detail import DetailView
@@ -15,20 +17,24 @@ class CandidateListDetailView(DetailView):
     model = CandidateList
 
     def get_context_data (self, **kwargs):
-        context = super(CandidateListDetailView, self).get_context_data(**kwargs)
-        cl = context['object']
-        context['head'] = cl.getHeadName()
-        candidates = Candidate.objects.select_related('person',
-                'person__mk').filter(candidates_list=cl).order_by('ordinal')
-        context['candidates'] = [x.person for x in candidates]
-        agendas = []
-        if cl.member_ids:
-            for a in Agenda.objects.filter(is_public=True).order_by('-num_followers'):
-                agendas.append({'id': a.id, 
-                                'name': a.name,
-                                'url': a.get_absolute_url(),
-                                'score': a.candidate_list_score(cl)})
-            context['agendas'] = agendas
+        cache_key = "candidate_list_%(pk)s" % kwargs
+        context = cache.get(cache_key)
+        if not context:
+            context = super(CandidateListDetailView, self).get_context_data(**kwargs)
+            cl = context['object']
+            context['head'] = cl.getHeadName()
+            candidates = Candidate.objects.select_related('person',
+                    'person__mk').filter(candidates_list=cl).order_by('ordinal')
+            context['candidates'] = [x.person for x in candidates]
+            agendas = []
+            if cl.member_ids:
+                for a in Agenda.objects.filter(is_public=True).order_by('-num_followers'):
+                    agendas.append({'id': a.id, 
+                                    'name': a.name,
+                                    'url': a.get_absolute_url(),
+                                    'score': a.candidate_list_score(cl)})
+                context['agendas'] = agendas
+            cache.set(cache_key, context, settings.LONG_CACHE_TIME)
         return context
 
 class CandidateListCompareView(TemplateView):
@@ -38,26 +44,29 @@ class CandidateListCompareView(TemplateView):
     template_name = "polyorg/candidatelist_compare.html"
 
     def get_context_data(self, **kwargs):
-        ctx = super(CandidateListCompareView, self).get_context_data(**kwargs)
+        ctx = cache.get("candidate_list_compare")
+        if not ctx:
+            ctx = super(CandidateListCompareView, self).get_context_data(**kwargs)
 
-        clists = [{'name': cl.name,
-                   'ballot': cl.ballot,
-                   'url': cl.get_absolute_url(),
-                   'wikipedia_page': cl.wikipedia_page,
-                   'facebook_url': cl.facebook_url,
-                   'candidates': [{'id': person.id,
-                                   'name': person.name,
-                                   'img_url': person.img_url,
-                                   'ordinal': None,  # TODO
-                                   'gender': person.gender or 'X',
-                                   'mk':getattr(person, "mk") is not None,
-                                   'bills_stats_approved': person.mk.bills_stats_approved if person.mk else None,
-                                   'bills_stats_proposed': person.mk.bills_stats_proposed if person.mk else None,
-                                   'residence_centrality': person.residence_centrality,
-                                   'role': person.roles.all()[0].text if person.roles.count() else None
-                                  }
-                                  for person in cl.candidates.order_by('candidate__ordinal')[:10]]}
-                  for cl in CandidateList.objects.order_by('ballot')]
+            clists = [{'name': cl.name,
+                       'ballot': cl.ballot,
+                       'url': cl.get_absolute_url(),
+                       'wikipedia_page': cl.wikipedia_page,
+                       'facebook_url': cl.facebook_url,
+                       'candidates': [{'id': person.id,
+                                       'name': person.name,
+                                       'img_url': person.img_url,
+                                       'ordinal': None,  # TODO
+                                       'gender': person.gender or 'X',
+                                       'mk':getattr(person, "mk") is not None,
+                                       'bills_stats_approved': person.mk.bills_stats_approved if person.mk else None,
+                                       'bills_stats_proposed': person.mk.bills_stats_proposed if person.mk else None,
+                                       'residence_centrality': person.residence_centrality,
+                                       'role': person.roles.all()[0].text if person.roles.count() else None
+                                      }
+                                      for person in cl.candidates.order_by('candidate__ordinal')[:10]]}
+                      for cl in CandidateList.objects.order_by('ballot')]
 
-        ctx['candidate_lists'] = json.dumps(clists)
+            ctx['candidate_lists'] = json.dumps(clists)
+            cache.set("candidate_list_compare", ctx, settings.LONG_CACHE_TIME)
         return ctx
