9156915147ed9c9931162ea9c5ca77a0ef931f37
Meir Kriheli <mkriheli@gmail.com>
2013-01-24 01:33:29 +0200

    Enable to hide label, don't bomb on invalid data

--- auxiliary/templatetags/common_visual_elements.py
+++ auxiliary/templatetags/common_visual_elements.py
@@ -5,21 +5,24 @@ register = template.Library()
 
 
 @register.inclusion_tag('auxiliary/_bar.html')
-def bar(quantity, start, end, bar_class=None):
+def bar(quantity, start, end, bar_class=None, show_label=True):
     """
     Draws a bar.
 
-    :param quantity: The quanity to represent
+    :param quantity: The quantity to represent
     :param start: Start of scale
     :param end: End of scale
     :param bar_class: Bootstrap bar class (One of: info, success, warning,
                       danger). If not passed, will be calculated from percentage
-
+    :param show_label: Show the descriptive label ?
     """
 
     assert quantity <= end, "bar: quantity > end"
 
-    value = (quantity - start) * 100 / (end - start)
+    try:
+        value = (quantity - start) * 100 / (end - start)
+    except (TypeError, ZeroDivisionError):
+        return {'applicable': False}
 
     VALUES = (
         (20, 'danger', _('Extremely below average')),
@@ -36,5 +39,8 @@ def bar(quantity, start, end, bar_class=None):
     if bar_class is None:
         bar_class = css_class
 
+    if not show_label:
+        label = ''
+
     return {'width': value, 'bar_class': bar_class, 'label': label,
-            'quantity': quantity}
+            'quantity': quantity, 'applicable': True}
