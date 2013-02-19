b45565093408de1f9ea0dd4699e80a4bbe775636
Meir Kriheli <mkriheli@gmail.com>
2013-02-19 13:18:41 +0200

    Add manager for current_knesset to members

--- mks/models.py
+++ mks/models.py
@@ -10,6 +10,8 @@ from planet.models import Blog
 from knesset.utils import cannonize
 from links.models import Link
 import difflib
+from mks.managers import (BetterManager, KnessetManager,
+                          CurrentKnessetMembersManager)
 
 GENDER_CHOICES = (
     (u'M', _('Male')),
@@ -27,42 +29,6 @@ class Correlation(models.Model):
     def __unicode__(self):
         return "%s - %s - %.0f" % (self.m1.name, self.m2.name, self.normalized_score)
 
-
-class BetterManager(models.Manager):
-    def __init__(self):
-        super(BetterManager, self).__init__()
-        self._names = []
-
-    def find(self, name):
-        ''' looks for a member with a name that resembles 'name'
-            the returned array is ordered by similiarity
-        '''
-        names = cache.get('%s_names' % self.model.__name__)
-        if not names:
-            names = self.values_list('name', flat=True)
-            cache.set('%s_names' % self.model.__name__, names)
-        possible_names = difflib.get_close_matches(
-            name, names, cutoff=0.5, n=5)
-        qs = self.filter(name__in=possible_names)
-        # used to establish size, overwritten later
-        ret = range(qs.count())
-        for m in qs:
-            if m.name == name:
-                return [m]
-            ret[possible_names.index(m.name)] = m
-        return ret
-
-class KnessetManager(models.Manager):
-    """This is a manager for Knesset class"""
-
-    def __init__(self):
-        super(KnessetManager, self).__init__()
-        self._current_knesset = None
-
-    def current_knesset(self):
-        if self._current_knesset is None:
-            self._current_knesset = self.get_query_set().order_by('-number')[0]
-        return self._current_knesset
 
 class CoalitionMembership(models.Model):
     party = models.ForeignKey('Party',
@@ -221,6 +187,7 @@ class Member(models.Model):
     backlinks_enabled = models.BooleanField(default=True)
 
     objects = BetterManager()
+    current_knesset = CurrentKnessetMembersManager()
 
     class Meta:
         ordering = ['name']
