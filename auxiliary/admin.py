a00850f468b42148d22b678aadc4d30960ab4f1c
Meir Kriheli <mkriheli@gmail.com>
2013-03-11 16:24:44 +0200

    active -> is_active tidbit admin

--- auxiliary/admin.py
+++ auxiliary/admin.py
@@ -6,9 +6,9 @@ from .models import Tidbit
 class TidibitAdmin(admin.ModelAdmin):
 
     model = Tidbit
-    list_display = ('title', 'content', 'ordering', 'active')
+    list_display = ('title', 'content', 'ordering', 'is_active')
     list_display_links = ('title', 'content')
-    list_editable = ('ordering', 'active')
+    list_editable = ('ordering', 'is_active')
 
 
 admin.site.register(Tidbit, TidibitAdmin)
