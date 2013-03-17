402ce97e544dcd50bd61c7850450770ad3cebbb8
Ori Hoch <ori@uumpa.com>
2013-03-17 16:47:08 +0200

    fix a bug where committee members are not displayed for plenum meetings

--- committees/views.py
+++ committees/views.py
@@ -122,10 +122,14 @@ class MeetingDetailView(DetailView):
         context['parts_lengths'] = json.dumps(parts_lengths)
         context['paginate_by'] = COMMITTEE_PROTOCOL_PAGINATE_BY
 
-        #get meeting members with presence calculation
-        meeting_members_ids = set(m.id for m in cm.mks_attended.all())
-        context['members'] = [m for m in cm.committee.members_by_presence()
-                              if m.id in meeting_members_ids]
+        if cm.committee.type=='plenum':
+            context['members']=cm.mks_attended.order_by('name')
+            context['memer_photos_no_presence']=True
+        else:
+            #get meeting members with presence calculation
+            meeting_members_ids = set(m.id for m in cm.mks_attended.all())
+            context['members'] = [m for m in cm.committee.members_by_presence()
+                                if m.id in meeting_members_ids]
 
         return context
 
