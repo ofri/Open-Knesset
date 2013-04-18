bf4432d674bc72bfec4d0df80c1684a237916efb
Meir Kriheli <mkriheli@gmail.com>
2013-04-18 11:00:59 +0300

    Transalte fields in Suggestion's __unicode__
    Better display of suggestion in the dialog by default

--- suggestions/models.py
+++ suggestions/models.py
@@ -145,11 +145,15 @@ class SuggestedAction(models.Model):
     def __unicode__(self):
         if self.subject_id:
             label = unicode(self.subject)
+            meta = self.subject._meta
         else:
             model = self.subject_type.model_class()
             label = unicode(model._meta.verbose_name)
+            meta = model._meta
 
-        fields = [_(f) + ': ' + unicode(v) for (f, v) in self.action_params]
+        s_fields = dict((x.name, x) for x in meta.fields)
+        fields = [unicode(s_fields.get(f).verbose_name)
+                  + ': ' + unicode(v) for (f, v) in self.action_params]
         res = u'{0} {1}: {2}'.format(self.get_action_display(), label,
                                      ', '.join(fields))
         return res
