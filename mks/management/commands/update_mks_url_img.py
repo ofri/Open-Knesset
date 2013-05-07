bffc403505e1f460fc6402f4eb32ed995a283a77
ofri <ofri.raviv@gmail.com>
2013-05-07 15:49:41 +0300

    some cleanup in update_mks_url_img command

--- mks/management/commands/update_mks_url_img.py
+++ mks/management/commands/update_mks_url_img.py
@@ -1,29 +1,25 @@
-import urllib2
-import re
+import urllib2, re, logging
 
-from django.core.management.base import NoArgsCommand,BaseCommand, CommandError
-#from django.core.files.base import ContentFile
-#from django.contrib.auth.models import User, Group
-
-#from actstream import follow
+from django.core.management.base import NoArgsCommand
 
 from mks.models import Member
-#from avatar.models import Avatar
+
+logger = logging.getLogger("open-knesset.mks.update_mks_url_img")
 
 class Command(NoArgsCommand):
-    help = "update member url_img"
+    help = "update all members url_img from knesset website"
 
     def handle_noargs(self, **options):
-        # TODO: ---
         for member in Member.objects.all():
             mk_id = member.id
-	    searchUrl='http://www.knesset.gov.il/mk/heb/ShowPic.asp?mk_individual_id_t=%s'%mk_id
-	    resUrl='mk/images/members/'
-	    page=urllib2.urlopen(searchUrl)
-	    data=page.read()
-	    resUrl='http://www.knesset.gov.il/'+re.search('mk/images/members/.*.jpg',data).group(0)
-	    member.url_img=resUrl
-	    member.save()
-
-	
-
+            search_url = 'http://www.knesset.gov.il/mk/heb/ShowPic.asp?mk_individual_id_t=%s' % mk_id
+            try:
+                page = urllib2.urlopen(search_url)
+                data = page.read()
+                res_url = 'http://www.knesset.gov.il/%s' % (
+                    re.search('mk/images/members/.*?.jpg',data).group(0))
+                member.img_url = res_url
+                member.save()
+            except:
+                logger.debug('couldn\'t update img_url for member %d' %
+                             member.id)
