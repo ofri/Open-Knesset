ee554899412dfc1773d3a7862e9f1b8625c967e7
Ori Hoch <ori@uumpa.com>
2013-09-30 19:10:34 +0300

    minor fixes, add support to bill_detail, remove old tagging js code

--- static/js/tagging.js
+++ static/js/tagging.js
@@ -1,102 +1,3 @@
-var selected_tags = {};
-var current_id = "";
-var current_object_type = "";
-var current_app = "";
-
-function get_tags_list(url) {
-    if (typeof(url) === 'undefined') {
-        url = '/api/v2/tag/?format=json&limit=100'
-    }
-    $.ajax({url: url, success: get_tags_list_callback, cache: false});
-}
-
-function add_nav_button(text, url) {
-    if (url !== null) {
-        $('<a class="btn btn-small">').attr('href', "javascript:get_tags_list('"+encodeURIComponent(url)+"')").html(text).appendTo("#possible_tags");
-    }
-}
-
-function get_tags_list_callback(data) {
-    $("#possible_tags").html('<br/>');
-    $.each(data['objects'], function(i,item){
-            var href = "javascript:toggle_tag("+item.id+");";
-            $('<a class="tag">').attr("id", "possible_tag_"+item.id).attr("href", href).html(item.name).appendTo("#possible_tags");
-            $("#possible_tags").append(document.createTextNode(" "));
-          });
-    meta = data['meta'];
-
-	if (meta) { 
-		$("#possible_tags").append('<br/>');
-		add_nav_button(gettext('previous'), meta['previous']);
-		add_nav_button(gettext('next'), meta['next']);
-	}
-    current_app = $('#add_tags').data('appLabel');
-    current_object_type = $('#add_tags').data('objectType');
-    current_id = $('#add_tags').data('id');
-    // currently committeemeeting urls have a unique structure. clean this up.
-    if (current_id == 'meeting') {
-        current_object_type = 'committeemeeting';
-        current_id = window.location.pathname.split('/')[3];
-    }
-    var url = '/api/v2/tag/'+current_app+'/'+current_object_type+'/'+current_id+'/';
-    $.ajax({url: url, success: mark_selected_tags, cache: false});
-    $('#create_tag').show();
-    $('#create_tag_form').submit(function(){
-        var csrf = $('input[name="csrfmiddlewaretoken"]').val();
-        var tag = $('#tag').val();
-        $.post("/tags/"+current_app+"/"+current_object_type+"/"+current_id+"/create-tag/",
-            { tag: tag, csrfmiddlewaretoken: csrf }, add_tag_callback );
-        return false;
-    });
-}
-
-function mark_selected_tags(data){
-    $.each(data['objects'], function(i,item){
-        $("#possible_tag_"+item.id).addClass("selected label");
-        selected_tags[item.id]=1;
-    });
-}
-
-function toggle_tag(tag_id) {
-    var csrf = $('input[name="csrfmiddlewaretoken"]').val();
-    if (tag_id in selected_tags){
-        $.post("/tags/"+current_app+"/"+current_object_type+"/"+current_id+"/remove-tag/",
-                { tag_id: tag_id, csrfmiddlewaretoken: csrf }, remove_tag_callback );
-        $("#possible_tag_"+tag_id).removeClass("selected label");
-        delete selected_tags[tag_id];
-    } else {
-        $.post("/tags/"+current_app+"/"+current_object_type+"/"+current_id+"/add-tag/",
-                { tag_id: tag_id, csrfmiddlewaretoken: csrf }, add_tag_callback );
-        $("#possible_tag_"+tag_id).addClass("selected label");
-        selected_tags[tag_id] = 1;
-    }
-}
-
-function add_tag_callback(data) {
-    var item = eval('('+data+')');
-    $('#no-tags-yet').remove();
-    if( $("#tag_"+item.id).length == 0 ){ // we don't already have this tag.
-        var href = item.url;
-        $('<a class="tag label">').attr("id", "tag_"+item.id).attr("href", href).html(item.name).appendTo("#tags");
-        $("#tags").append(document.createTextNode(" "));
-    };
-    if( $("#possible_tag_"+item.id).length == 0 ){ // we don't already have this tag in the possible list.
-        href = "javascript:toggle_tag("+item.id+");";
-        $('<a class="tag">').attr("id", "possible_tag_"+item.id).attr("href", href).html(item.name).appendTo("#possible_tags");
-        $("#possible_tags").append(document.createTextNode(" "));
-    };
-    $("#possible_tag_"+item.id).addClass("selected");
-    selected_tags[item.id]=1;
-}
-
-function remove_tag_callback(data) {
-    var item = eval('('+data+')');
-    $('#tag_'+item.id).remove();
-}
-
-/*** new tags ***/
-/*** currently only for committeemeeting ***/
-
 window.tags={
     init:function(data){
         this._model.init(
@@ -117,7 +18,8 @@ window.tags={
         var name=_this._view.getAddTagName();
         if (name.length>0 && _this._view.lock()) {
             /*
-                there are bugs with trying to add tag id directly
+                I wanted to add tag using it's id to prevent an extra ajax call
+                but there are bugs with it
                 disabled for now, need to investigate further
             */
             var id=false;//_this._view.getAddTagId();
@@ -299,6 +201,11 @@ window.tags={
                     serviceUrl: '/api/v2/tag/',
                     params: {limit:50,jquery_autocomplete:1},
                     paramName: 'name__startswith',
+                });
+                $('#tags_addnew input').on('keyup',function(e){
+                    if (e.which==13) {
+                        tags.add();
+                    };
                 });
                 $('#no-tags-yet').hide();
             };
