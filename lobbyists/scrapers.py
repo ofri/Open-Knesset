4d3ca0b3130696e8ee8ee60da7d24d1415586a41
Ori Hoch <ori@uumpa.com>
2014-02-03 23:02:54 +0200

    180 lobbyists scraper is ready!

--- lobbyists/scrapers.py
+++ lobbyists/scrapers.py
@@ -3,22 +3,67 @@
 from bs4 import BeautifulSoup
 from okscraper.base import BaseScraper
 from okscraper.sources import UrlSource, ScraperSource
-from okscraper.storages import DictStorage, ListStorage
-import okscraper.djangoStorages as djangoStorages
+from okscraper.storages import ListStorage
 from models import Lobbyist, LobbyistRepresent
 from persons.models import Person
+from okscraper.django.storages.model import ModelDictStorage, ModelListDictStorage
+import okscraper.django.storages.fields as okscraper_django_fields
+import okscraper.django.storages.source_fields as okscraper_django_source_fields
+
+
+class MainScraper(BaseScraper):
+
+    def __init__(self):
+        super(MainScraper, self).__init__(self)
+        self.source = ScraperSource(LobbyistsIndexScraper())
+        self.storage = ListStorage()
+
+    def _scrape(self):
+        lobbyist_ids = self.source.fetch()
+        for lobbyist_id in lobbyist_ids:
+            lobbyistScraper = LobbyistScraper()
+            lobbyist = lobbyistScraper.scrape(lobbyist_id)
+            representScraper = LobbyistRepresentScraper()
+            lobbyist_represents = representScraper.scrape(lobbyist_id, lobbyist)
+            self.storage.store((lobbyist, lobbyist_represents))
+
 
 class LobbyistScraper(BaseScraper):
 
     def __init__(self):
         super(LobbyistScraper, self).__init__()
         self.source = UrlSource('http://online.knesset.gov.il/WsinternetSps/KnessetDataService/LobbyistData.svc/View_lobbyist(<<id>>)')
-        self.storage = djangoStorages.ModelDictStorage(Lobbyist, {
-            'person': djangoStorages.ModelValueRelated(Person, {
-                'name': djangoStorages.ModelValueStringConcat('first_name', 'family_name')
-            }),
-            'knesset_id': 'id'
-        })
+        self.storage = ModelDictStorage(
+            fields_processor = okscraper_django_fields.FieldsProcessor(
+                model = Lobbyist,
+                fields = [
+                    okscraper_django_fields.Simple(
+                        model_field = 'person',
+                        source_field = okscraper_django_source_fields.Related(
+                            fields_processor = okscraper_django_fields.FieldsProcessor(
+                                model = Person,
+                                fields = [
+                                    okscraper_django_fields.Simple(
+                                        model_field = 'name',
+                                        source_field = okscraper_django_source_fields.StringFormat(
+                                            string_format = '%s %s',
+                                            field_names = ['first_name', 'family_name']
+                                        )
+                                    ),
+                                ]
+                            )
+                        )
+                    ),
+                    okscraper_django_fields.Primary(model_field = 'knesset_id', source_field = 'id'),
+                    'profession',
+                    'corporation_name',
+                    'corporation_id',
+                    'faction_member',
+                    'faction_name',
+                    'permit_type',
+                ]
+            )
+        )
 
     def _storeLobbyistDataFromSoup(self, soup):
         lobbyist_id = soup.find('d:lobbyist_id').text.strip()
@@ -47,10 +92,18 @@ class LobbyistRepresentScraper(BaseScraper):
     def __init__(self):
         super(LobbyistRepresentScraper, self).__init__(self)
         self.source = UrlSource('http://online.knesset.gov.il/WsinternetSps/KnessetDataService/LobbyistData.svc/View_lobbyist(<<id>>)/lobbist_type')
-        self.storage = djangoStorages.ModelListDictStorage(LobbyistRepresent, {
-            'knesset_id': 'id',
-            'lobbyist_id': djangoStorages.ModelValueNone('lobbyist_id')
-        })
+        self.storage = ModelListDictStorage(
+            fields_processor = okscraper_django_fields.FieldsProcessor(
+                model = LobbyistRepresent,
+                fields = [
+                    okscraper_django_fields.Primary(model_field = 'knesset_id', source_field = 'id'),
+                    okscraper_django_fields.ManyToMany(source_field = 'lobbyist', related_field_name = 'represents'),
+                    'name',
+                    'domain',
+                    'type',
+                ]
+            )
+        )
 
     def _storeLobbyistRepresentDataFromSoup(self, soup, lobbyist_id, lobbyist = None):
         self._getLogger().info('got lobbyist represent (lobbyist id "%s")', lobbyist_id)
@@ -62,32 +115,17 @@ class LobbyistRepresentScraper(BaseScraper):
             represent['domain'] = elt.find('d:lobbyist_represent_domain').text.strip()
             represent['type'] = elt.find('d:lobbyist_represent_type').text.strip()
             self._getLogger().debug(represent)
+            if lobbyist is not None:
+                represent['lobbyist'] = lobbyist
             self.storage.store(represent)
-        if lobbyist is not None:
-            for lobbyist_represent in self.storage.get():
-                lobbyist.represents.add(lobbyist_represent)
-            lobbyist.save()
+            #for lobbyist_represent in self.storage.get():
+            #    lobbyist.represents.add(lobbyist_represent)
+            #lobbyist.save()
 
     def _scrape(self, lobbyist_id, lobbyist = None):
         html = self.source.fetch(lobbyist_id)
         soup = BeautifulSoup(html)
         return self._storeLobbyistRepresentDataFromSoup(soup, lobbyist_id, lobbyist)
-
-class LobbyistsScraper(BaseScraper):
-
-    def __init__(self):
-        super(LobbyistsScraper, self).__init__(self)
-        self.source = ScraperSource(LobbyistsIndexScraper())
-        self.storage = ListStorage()
-
-    def _scrape(self):
-        lobbyist_ids = self.source.fetch()
-        for lobbyist_id in lobbyist_ids:
-            lobbyistScraper = LobbyistScraper()
-            lobbyist = lobbyistScraper.scrape(lobbyist_id)
-            representScraper = LobbyistRepresentScraper()
-            lobbyist_represents = representScraper.scrape(lobbyist_id, lobbyist)
-            self.storage.store((lobbyist, lobbyist_represents))
 
 class LobbyistsIndexScraper(BaseScraper):
 
@@ -111,15 +149,3 @@ class LobbyistsIndexScraper(BaseScraper):
         html = self.source.fetch()
         soup = BeautifulSoup(html)
         return self._storeLobbyistIdsFromSoup(soup)
-
-
-okscrapers = [
-    LobbyistScraper,
-    LobbyistRepresentScraper,
-    LobbyistsScraper,
-    LobbyistsIndexScraper
-]
-
-default_okscrapers = [
-    LobbyistsScraper
-]
