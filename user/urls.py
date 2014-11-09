fbf18a56240ed60c165ba6d19066963e44bac475
Amir More <habeanf@gmail.com>
2014-11-09 11:33:31 +0200

    Bug fix: update url param for password reset confirm to match change in django 1.6 See changes in 1.6 as documented at https://docs.djangoproject.com/en/1.6/topics/auth/default/#django.contrib.auth.views.password_reset

--- user/urls.py
+++ user/urls.py
@@ -21,7 +21,7 @@ urlpatterns += patterns('django.contrib.auth.views',
     url(r'^logout/$', 'logout_then_login', name='logout'),
     url(r'^password_reset/$', 'password_reset', {'template_name': 'user/password_reset_form.html'}, name='password_reset'),
     url(r'^password_reset/done/$', 'password_reset_done', {'template_name': 'user/password_reset_done.html'}, name='password_reset_done'),
-    url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm', {'template_name': 'user/password_reset_confirm.html'}),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm', {'template_name': 'user/password_reset_confirm.html'}),
     url(r'^reset/done/$', 'password_reset_complete', {'template_name': 'user/password_reset_complete.html'}),
     )
 
