6b9e300f8df7e1f6efd4de75bbfd7cf5b6c75907
Benny Daon <bennydaon@gmail.com>
2015-02-02 12:33:17 +0200

    Adding the candidates in the 2015 election
    To load the data from the official site:
        `python manage.py parse_candidates.py`
    It should run only once

--- simple/management/commands/parse_candidates.py
+++ simple/management/commands/parse_candidates.py
@@ -2,46 +2,50 @@
 # coding: utf8
 
 import urllib2
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
 import logging
+from datetime import datetime
 from django.core.management.base import BaseCommand
+from persons.models import Person, PersonAlias, Role
 
 logger = logging.getLogger("open-knesset.parse_candidates")
 
 class Parties(object):
 
-	def __init__(self, url, attr_name, attr_val):
+    def __init__(self, url):
         self.url = url
-        self.attr_name = attr_name
-        self.attr_val = attr_val
 
-	def __iter__(self):
-        return self
-
-	def import_party(self, party_name, link):
-		data = BeautifulSoup(urllib2.urlopen(url).read())
+    def import_party(self, name, link):
+        print (u"loading {} candidates from {}".format(name, link))
+        data = BeautifulSoup(urllib2.urlopen(link).read())
+        # lose the leading ה from the party name
+        party_name = name[1:] if name[0] == u'ה' else name
         ordinal = 1
-		for person_name in data.find_all(attrs={"class": "candidate"}):
+        for i in data.find_all(attrs={"class": "candidate"}):
+            person_name = i.string
             try:
                 person = Person.objects.get(name=person_name)
-            except KeyError:
+            except Person.DoesNotExist:
                 try:
                     person = PersonAlias.objects.get(name=person_name).person
-                except KeyError:
+                except PersonAlias.DoesNotExist:
                     person = Person.objects.create(name=person_name)
 
-            person.roles.add(start_date=datetime(2015, 1, 29),
-                             org="הבחירות לכנסת ה-20",
-                             text="מקום {} ב{}".format(ordinal, party_name))
+
+            role, created = Role.objects.get_or_create(start_date=datetime(2015, 1, 29),
+                             org=u"הבחירות לכנסת ה-20",
+                             person=person,
+                             text=u"מקום {} ב{}".format(ordinal, party_name))
+            if created:
+                print (u"created a role for {}".format(person_name))
             ordinal += 1
 
-    def parse(self)
-		data = BeautifulSoup(urllib2.urlopen(self.url).read())
-        return imap (self.import_party,
-                     data.find_all(attrs={"class": "candidates-title-k"}),
-                     data.find_all(attrs={"class": "internallinkKnessetTitle"}))
-
+    def parse(self):
+        data = BeautifulSoup(urllib2.urlopen(self.url).read())
+        for i in data.find_all(attrs={"class": "internallinkKnessetTitle"}):
+            self.import_party(i.string,
+                "http://bechirot.gov.il/election/Candidates/Pages/{}".format(i.a["href"]))
 
 class Command(BaseCommand):
     """Parse Election candidates from their webpage http://bechirot.gov.il/election/Pages/CandidatesList.aspx"""
@@ -51,9 +55,6 @@ class Command(BaseCommand):
     base_url = "http://bechirot.gov.il/election/Candidates/Pages/default.aspx"
 
     def handle(self, *args, **options):
-        r = self.parse_committee_members()
-        logger.debug(r)
-        self.update_committee_members_db(r)
 
         logger.info("reading html from {}".format(self.base_url))
         parties = Parties(self.base_url)
