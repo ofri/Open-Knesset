92ac4f387db3d0b7462243d4b973d26ec33a9348
Benny Daon <bennydaon@gmail.com>
2015-02-05 12:54:34 +0200

    fixing person tests

--- persons/tests.py
+++ persons/tests.py
@@ -4,33 +4,33 @@ from django.test import TestCase
 from unittest import skip
 
 from .models import Person
-from mks.models import Member
+from mks.models import Member, Knesset
 
 
 class PersonTests(TestCase):
-    def test_member_person_sync(self):
-        """
-        Test member/person sync on member save()
-        """
-
-        birth = date.today()
-
-        defaults = {
+    def setUp(self):
+        self.knesset = Knesset.objects.create(
+            number=1,
+            start_date=date(2014,1,1))
+        self.defaults = {
             'name': 'The MK',
-            'date_of_birth': birth,
+            'date_of_birth': date.today(),
             'family_status': 'XYZ',
             'place_of_residence': 'AAA',
             'phone': '000-1234',
             'fax': '999-8765',
             'gender': 'F',
         }
-        mk = Member.objects.create(**defaults)
+
+    def test_member_person_sync(self):
+        """ Test member/person sync on member save() """
+        mk = Member.objects.create(**self.defaults)
 
         self.assertGreater(mk.person.count(), 0)
 
         person = Person.objects.filter(mk=mk)[0]
 
-        for field in defaults:
+        for field in self.defaults:
             self.assertEqual(getattr(mk, field), getattr(person, field))
 
         mk.delete()
