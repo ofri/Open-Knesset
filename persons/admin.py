cdafb8ab7c63bffdcc4bddebf86afecd86f0b6dd
Benny Daon <bennydaon@gmail.com>
2015-02-05 14:29:38 +0200

    Adding 'merge persons' action in admin

--- persons/admin.py
+++ persons/admin.py
@@ -1,8 +1,44 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from links.models import Link
 from models import Person,PersonAlias,Title,Role
 
 class RoleInline(admin.TabularInline):
     model = Role
+
+def merge_persons(modeladmin, request, qs):
+    pivot = None
+    for person in qs:
+        if person.mk:
+            pivot = person
+            break
+    if not pivot:
+        pivot = qs[0]
+
+    for person in qs:
+        if person != pivot:
+            for role in person.roles.all():
+                role.person = pivot
+                role.save()
+            for alias in person.aliases.all():
+                alias.person = pivot
+                alias.save()
+            for pp in person.protocol_parts.all():
+                pp.speaker = pivot
+                pp.save()
+            for link in Link.objects.for_model(person):
+                link.content_object = pivot
+                link.save()
+            for field in person._meta.fields:
+                if field in ['id']:
+                    continue
+                field_name = field.get_attname()
+                val = getattr(person, field_name)
+                if val and not getattr(pivot, field_name):
+                    setattr(pivot, field_name, val)
+            person.delete()
+    pivot.save()
+merge_persons.short_description = _("Merge two or more persons")
 
 class PersonAdmin(admin.ModelAdmin):
     list_display=('name', 'mk', 'user')
@@ -12,6 +48,7 @@ class PersonAdmin(admin.ModelAdmin):
         RoleInline,
     ]
     readonly_fields = ('calendar_sync_token',)
+    actions = [merge_persons]
 
 admin.site.register(Person, PersonAdmin)
 
@@ -28,3 +65,4 @@ class RoleAdmin(admin.ModelAdmin):
     ordering = ('person',)
     list_display = ('person','text')
 admin.site.register(Role, RoleAdmin)
+
--- persons/tests.py
+++ persons/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from unittest import skip
 
 from .models import Person
+from .admin import merge_persons
 from mks.models import Member, Knesset
 
 
@@ -13,14 +14,31 @@ class PersonTests(TestCase):
             number=1,
             start_date=date(2014,1,1))
         self.defaults = {
-            'name': 'The MK',
+            'name': 'a name',
             'date_of_birth': date.today(),
             'family_status': 'XYZ',
             'place_of_residence': 'AAA',
             'phone': '000-1234',
-            'fax': '999-8765',
             'gender': 'F',
         }
+
+    def test_merge_persons(self):
+        mk = Member.objects.create()
+        mk_person = mk.person.all()[0]
+        person = Person.objects.create(**self.defaults)
+        person.roles.create(org="the org", text="title")
+
+        merge_persons(None, None, Person.objects.filter(
+            id__in=[person.id, mk_person.id]))
+        with self.assertRaises(Person.DoesNotExist):
+            Person.objects.get(id=person.id)
+
+        for k,v in self.defaults.items():
+            self.assertEqual(getattr(person, k), v)
+        roles = mk_person.roles.all()
+        self.assertEquals(roles.count(), 1)
+        role = roles[0]
+        self.assertEquals(role.org, "the org")
 
     def test_member_person_sync(self):
         """ Test member/person sync on member save() """
