135797f6c2b5c93346d897b0a37ba784dde80f43
Benny Daon <bennydaon@gmail.com>
2015-02-10 14:23:06 +0200

    importing the marker candidates csv file

--- persons/managers.py
+++ persons/managers.py
@@ -1,9 +1,14 @@
 from django.utils.encoding import smart_text
-from django.db.models import Q, Manager
+from django.db.models import Q, Manager, Model
+from django.core.exceptions import MultipleObjectsReturned
 
 class PersonManager(Manager):
 
     def get_by_name(self, name):
         name = smart_text(name)
-        return self.get((Q(aliases__name__in=name) | Q(name=name)))
+        try:
+            return self.get((Q(aliases__name__in=name) | Q(name=name)))
+        except MultipleObjectsReturned:
+            ''' just take the first one '''
+            return self.filter((Q(aliases__name__in=name) | Q(name=name)))[0]
 
